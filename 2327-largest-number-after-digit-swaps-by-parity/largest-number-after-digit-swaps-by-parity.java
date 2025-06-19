a1eda16835e7376659d7c51e3bc92bcf17fabf9b
Hedayatullah Yamin <67416501+Munib03@users.noreply.github.com>
2025-06-19 09:19:29 +0430

    Time: 4 ms (7.51%) | Memory: 40.7 MB (65.86%) - LeetSync

--- 2327-largest-number-after-digit-swaps-by-parity/largest-number-after-digit-swaps-by-parity.java
+++ 2327-largest-number-after-digit-swaps-by-parity/largest-number-after-digit-swaps-by-parity.java
@@ -1,34 +1,35 @@
 class Solution {
     public int largestInteger(int num) {
+        var pq1 = new PriorityQueue<Integer>(Collections.reverseOrder());
+        var pq2 = new PriorityQueue<Integer>(Collections.reverseOrder());
+
         var sb = new StringBuilder();
         sb.append(num);
 
         for (var i = 0; i < sb.length(); i++) {
             var me = Integer.parseInt(sb.charAt(i) + "");
-            var he = 0;
-            var index = 0;
 
-            for (var j = i + 1; j < sb.length(); j++) {
-                var ou = Integer.parseInt(sb.charAt(j) + "");
+            if (me % 2 == 0)
+                pq1.offer(me);
 
-                if (me % 2 == 0 && ou % 2 == 0 && ou > he) {
-                    he = ou;
-                    index = j;
+            else
+                pq2.offer(me);
+        }
+
+        for (var i = 0; i < sb.length(); i++) {
+            var me = Integer.parseInt(sb.charAt(i) + "");
+
+            if (me % 2 == 0) {
+                if (!pq1.isEmpty()) {
+                    sb.setCharAt(i, (pq1.remove() + "").charAt(0));
                 }
-
-                else if (me % 2 == 1 && ou % 2 == 1 && ou > he) {
-                    he = ou;
-                    index = j;
+            } else {
+                if (!pq2.isEmpty()) {
+                    sb.setCharAt(i, (pq2.remove() + "").charAt(0));
                 }
-            }
-
-            if (he > me) {
-                sb.setCharAt(i, (he + "").charAt(0));
-                sb.setCharAt(index, (me + "").charAt(0));
             }
         }
 
         return Integer.parseInt(sb.toString());
     }
-
 }
