39e0d5e8a2f1e37f31818df7a82810bfb5a99936
Hedayatullah Yamin <67416501+Munib03@users.noreply.github.com>
2025-06-21 21:21:15 +0430

    Time: 7 ms (64.65%) | Memory: 44.8 MB (56.01%) - LeetSync

--- 692-top-k-frequent-words/top-k-frequent-words.java
+++ 692-top-k-frequent-words/top-k-frequent-words.java
@@ -1,41 +1,32 @@
 class Solution {
-
     public List<String> topKFrequent(String[] words, int k) {
-        var map = addToMap(words);
-        var sortedMap = sortMapByValue(map);
+        var map = new HashMap<String, Integer>();
+
+        for (var word : words)
+            map.put(word, map.getOrDefault(word, 0) + 1);
+
+        var pq = new PriorityQueue<String>((a, b) -> {
+            var freqCompare = map.get(a) - map.get(b);
+
+            if (freqCompare == 0)
+                return b.compareTo(a);
+
+            return freqCompare;
+        });
+
+        for (var word : map.keySet()) {
+            pq.offer(word);
+
+            if (pq.size() > k)
+                pq.poll();
+        }
 
         var list = new ArrayList<String>();
-        for (var val : sortedMap.keySet()) {
-            list.add(val);
-            if (--k == 0)
-                break;
-        }
+        while (!pq.isEmpty())
+            list.add(pq.remove());
+
+        Collections.reverse(list);
 
         return list;
     }
-
-    private HashMap<String, Integer> addToMap(String[] words) {
-        var map = new HashMap<String, Integer>();
-        for (var word : words)
-            map.put(word, map.getOrDefault(word, 0) + 1);
-        return map;
-    }
-
-    public LinkedHashMap<String, Integer> sortMapByValue(Map<String, Integer> map) {
-        var list = new ArrayList<>(map.entrySet());
-
-        list.sort((a, b) -> {
-            int cmp = b.getValue().compareTo(a.getValue());
-            if (cmp == 0)
-                return a.getKey().compareTo(b.getKey());
-            return cmp;
-        });
-
-        var sortedMap = new LinkedHashMap<String, Integer>();
-        for (var entry : list)
-            sortedMap.put(entry.getKey(), entry.getValue());
-
-        return sortedMap;
-    }
-
 }
