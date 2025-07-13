af345f222b0e6ee55e09061930ae0094512f54c6
Hedayatullah Yamin <67416501+Munib03@users.noreply.github.com>
2025-07-13 14:10:54 +0430

    Time: 8 ms (60.50%) | Memory: 43 MB (44.70%) - LeetSync

--- Hashing_Problems/567-permutation-in-string/permutation-in-string.java
+++ Hashing_Problems/567-permutation-in-string/permutation-in-string.java
@@ -4,18 +4,30 @@ class Solution {
             return false;
 
         int[] s1arr = new int[26];
-        for (int i = 0; i < s1.length(); i++)
+        int[] s2arr = new int[26];
+
+        for (int i = 0; i < s1.length(); i++) {
             s1arr[s1.charAt(i) - 'a']++;
-
-        for (int i = 0; i <= s2.length() - s1.length(); i++) {
-            int[] s2arr = new int[26];
-            for (int j = 0; j < s1.length(); j++)
-                s2arr[s2.charAt(i + j) - 'a']++;
-
-            if (Arrays.equals(s1arr, s2arr))
-                return true;
+            s2arr[s2.charAt(i) - 'a']++;
         }
 
-        return false;
+        for (int i = 0; i < s2.length() - s1.length(); i++) {
+            if (matches(s1arr, s2arr))
+                return true;
+
+            s2arr[s2.charAt(i + s1.length()) - 'a']++;
+            s2arr[s2.charAt(i) - 'a']--;
+        }
+
+        return matches(s1arr, s2arr);
     }
+
+    public boolean matches(int[] s1arr, int[] s2arr) {
+        for (int i = 0; i < 26; i++) {
+            if (s1arr[i] != s2arr[i])
+                return false;
+        }
+        return true;
+    }
+
 }
