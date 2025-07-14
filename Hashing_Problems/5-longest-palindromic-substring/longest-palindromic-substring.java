3f0bf833e8f4c18fd8d78d0a37dc1f5baf81313b
Hedayatullah Yamin <67416501+Munib03@users.noreply.github.com>
2025-07-14 11:17:35 +0430

    Time: 15 ms (85.78%) | Memory: 42.4 MB (71.14%) - LeetSync

--- Hashing_Problems/5-longest-palindromic-substring/longest-palindromic-substring.java
+++ Hashing_Problems/5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,11 +1,13 @@
 class Solution {
     public String longestPalindrome(String s) {
-        int start = 0, end = 0;
+        var start = 0;
+        var end = 0;
 
-        for (int i = 0; i < s.length(); i++) {
-            int len1 = expand(s, i, i);
-            int len2 = expand(s, i, i + 1);
-            int len = Math.max(len1, len2);
+        for (var i = 0; i < s.length(); i++) {
+            var len1 = expand(s, i, i);
+            var len2 = expand(s, i, i + 1);
+
+            var len = Math.max(len1, len2);
 
             if (len > end - start) {
                 start = i - (len - 1) / 2;
@@ -21,6 +23,7 @@ class Solution {
             left--;
             right++;
         }
+
         return right - left - 1;
     }
 }
