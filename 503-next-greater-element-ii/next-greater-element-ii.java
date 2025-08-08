c40418819dfab7299a5d6c264b31ec739c7f97ea
Hedayatullah Yamin <67416501+Munib03@users.noreply.github.com>
2025-08-08 15:26:54 +0430

    Time: 89 ms (9.97%) | Memory: 45.9 MB (89.48%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.java
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -1,35 +1,24 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
         var n = nums.length;
+
         int[] ans = new int[n];
 
-        var monotonicStack = new Stack<Integer>();
+        for (var i = 0; i < n; i++) {
+            var flag = false;
 
-        for (var i = n - 1; i >= 0; i--) {
-            var curr = nums[i];
+            for (var j = i + 1; j < n + i; j++) {
+                var index = j % n;
 
-            if (monotonicStack.isEmpty())
-                monotonicStack.push(curr);
-            else {
-                while (!monotonicStack.isEmpty() && monotonicStack.peek() <= curr)
-                    monotonicStack.pop();
-
-                monotonicStack.push(curr);
+                if (nums[index] > nums[i]) {
+                    flag = true;
+                    ans[i] = nums[index];
+                    break;
+                }
             }
-        }
 
-        for (var i = n - 1; i >= 0; i--) {
-            var curr = nums[i];
-
-            while (!monotonicStack.isEmpty() && monotonicStack.peek() <= curr)
-                monotonicStack.pop();
-
-            if (monotonicStack.isEmpty())
+            if (!flag)
                 ans[i] = -1;
-            else
-                ans[i] = monotonicStack.peek();
-
-            monotonicStack.push(curr);
         }
 
         return ans;
