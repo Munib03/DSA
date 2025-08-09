82aed89f57490b48cee84f3aaf8a65cf714bc774
Hedayatullah Yamin <67416501+Munib03@users.noreply.github.com>
2025-08-09 09:35:36 +0430

    Time: 43 ms (19.05%) | Memory: 44.2 MB (91.32%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.java
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.java
@@ -1,20 +1,123 @@
 class Solution {
-    public long subArrayRanges(int[] nums) {
-        var n = nums.length;
-        var sum = 0L;
+public long subArrayRanges(int[] nums) {
+  var ssmins = sumSubarrayMins(nums);
+  var ssmaxes = sumSubarrayMaxes(nums);
 
-        for (var i = 0; i < n; i++) {
-            var min = nums[i];
-            var max = nums[i];
+  return ssmaxes - ssmins;
+}
 
-            for (var j = i; j < n; j++) {
-                min = Math.min(nums[j], min);
-                max = Math.max(nums[j], max);
+public long sumSubarrayMins(int[] arr) {
+  var n = arr.length;
 
-                sum += max - min;
-            }
-        }
+  var pse = previousSmallerElementIndexes(arr);
+  var nse = nextSmallerElementIndexes(arr);
 
-        return sum;
-    }
+  var sum = 0L;
+
+  for (var i = 0; i < n; i++) {
+    var left = i - pse[i];
+    var right = nse[i] - i;
+
+    long prod = ((long) left * right) * arr[i];
+    sum += prod;
+  }
+
+  return sum;
+}
+
+public long sumSubarrayMaxes(int[] arr) {
+  var n = arr.length;
+
+  var pse = previousGreaterElement(arr);
+  var nse = nextGreaterElementIndexes(arr);
+
+  var sum = 0L;
+
+  for (var i = 0; i < n; i++) {
+    var left = i - pse[i];
+    var right = nse[i] - i;
+
+    long prod = ((long) left * right) * arr[i];
+    sum += prod;
+  }
+
+  return sum;
+}
+
+public int[] nextGreaterElementIndexes(int[] nums) {
+  var n = nums.length;
+
+  int[] ans = new int[n];
+  var monotonicStack = new Stack<Integer>();
+
+  for (var i = n - 1; i >= 0; i--) {
+    var num = nums[i];
+
+    // [1, 2, 3, 2, 5]
+    while (!monotonicStack.isEmpty() && num > nums[monotonicStack.peek()])
+      monotonicStack.pop();
+
+    ans[i] = (monotonicStack.isEmpty()) ? n : monotonicStack.peek();
+    monotonicStack.push(i);
+  }
+
+  return ans;
+}
+
+public int[] previousGreaterElement(int[] nums) {
+  var n = nums.length;
+
+  int[] ans = new int[n];
+  var monotonicStack = new Stack<Integer>();
+
+  for (var i = 0; i < n; i++) {
+    var num = nums[i];
+
+    while (!monotonicStack.isEmpty() && num >= nums[monotonicStack.peek()])
+      monotonicStack.pop();
+
+    ans[i] = (monotonicStack.isEmpty()) ? -1 : monotonicStack.peek();
+    monotonicStack.push(i);
+  }
+
+  return ans;
+}
+
+public int[] previousSmallerElementIndexes(int[] nums) {
+  var n = nums.length;
+
+  int[] ans = new int[n];
+  var monotonicStack = new Stack<Integer>();
+
+  for (var i = 0; i < n; i++) {
+    var num = nums[i];
+
+    while (!monotonicStack.isEmpty() && num < nums[monotonicStack.peek()])
+      monotonicStack.pop();
+
+    ans[i] = (monotonicStack.isEmpty()) ? -1 : monotonicStack.peek();
+    monotonicStack.push(i);
+  }
+
+  return ans;
+}
+
+public int[] nextSmallerElementIndexes(int[] nums) {
+  var n = nums.length;
+
+  int[] ans = new int[n];
+  var monotonicStack = new Stack<Integer>();
+
+  for (var i = n - 1; i >= 0; i--) {
+    var num = nums[i];
+
+    while (!monotonicStack.isEmpty() && num <= nums[monotonicStack.peek()])
+      monotonicStack.pop();
+
+    ans[i] = (monotonicStack.isEmpty()) ? n : monotonicStack.peek();
+    monotonicStack.push(i);
+  }
+
+  return ans;
+}
 }
