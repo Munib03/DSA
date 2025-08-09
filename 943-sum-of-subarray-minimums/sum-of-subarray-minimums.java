c331d401d63d4624285d8629a14f1a1398e9181a
Hedayatullah Yamin <67416501+Munib03@users.noreply.github.com>
2025-08-09 09:05:03 +0430

    Time: 77 ms (8.67%) | Memory: 51.2 MB (52.84%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
@@ -1,65 +1,57 @@
 class Solution {
     public int sumSubarrayMins(int[] arr) {
         var n = arr.length;
+
+        var pse = previousSmallerElementIndexes(arr);
+        var nse = nextSmallerElementIndexes(arr);
+
         var mod = (int) 1e9 + 7;
+        var sum = 0;
 
-        var nse = nextSmallerElement(arr);
-        var pse = previousSmallerElement(arr);
-
-        var totalAns = 0;
-
-        for (int i = 0; i < n; i++) {
-            int left = i - pse[i];
-            int right = nse[i] - i;
+        for (var i = 0; i < n; i++) {
+            var left = i - pse[i];
+            var right = nse[i] - i;
 
             long prod = ((long) left * right) % mod;
             prod = (prod * arr[i]) % mod;
-            totalAns = (int) ((totalAns + prod) % mod);
+            sum = (int) ((sum + prod) % mod);
         }
 
-        return totalAns;
+        return sum;
     }
 
-    public int[] nextSmallerElement(int[] nums) {
-        var n = nums.length;
-
-        int[] ans = new int[n];
-        var monotonicStack = new Stack<Integer>();
-
-        for (var i = n - 1; i >= 0; i--) {
-            var currNum = nums[i];
-
-            while (!monotonicStack.isEmpty() && currNum <= nums[monotonicStack.peek()])
-                monotonicStack.pop();
-
-            if (monotonicStack.isEmpty())
-                ans[i] = n;
-            else
-                ans[i] = monotonicStack.peek();
-
-            monotonicStack.push(i);
-        }
-
-        return ans;
-    }
-
-    public int[] previousSmallerElement(int[] nums) {
+    public int[] previousSmallerElementIndexes(int[] nums) {
         var n = nums.length;
 
         int[] ans = new int[n];
         var monotonicStack = new Stack<Integer>();
 
         for (var i = 0; i < n; i++) {
-            var currNum = nums[i];
+            var num = nums[i];
 
-            while (!monotonicStack.isEmpty() && currNum < nums[monotonicStack.peek()])
+            while (!monotonicStack.isEmpty() && num < nums[monotonicStack.peek()])
                 monotonicStack.pop();
 
-            if (monotonicStack.isEmpty())
-                ans[i] = -1;
-            else
-                ans[i] = monotonicStack.peek();
+            ans[i] = (monotonicStack.isEmpty()) ? -1 : monotonicStack.peek();
+            monotonicStack.push(i);
+        }
 
+        return ans;
+    }
+
+    public int[] nextSmallerElementIndexes(int[] nums) {
+        var n = nums.length;
+
+        int[] ans = new int[n];
+        var monotonicStack = new Stack<Integer>();
+
+        for (var i = n - 1; i >= 0; i--) {
+            var num = nums[i];
+
+            while (!monotonicStack.isEmpty() && num <= nums[monotonicStack.peek()])
+                monotonicStack.pop();
+
+            ans[i] = (monotonicStack.isEmpty()) ? n : monotonicStack.peek();
             monotonicStack.push(i);
         }
 
