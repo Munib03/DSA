c2ac062e11bef9088e8a2d79d84632462b801973
Hedayatullah Yamin <67416501+Munib03@users.noreply.github.com>
2025-08-12 10:57:43 +0430

    Time: 8 ms (15.37%) | Memory: 45.7 MB (19.49%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.java
+++ 735-asteroid-collision/asteroid-collision.java
@@ -1,27 +1,30 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
+        var n = asteroids.length;
+
         var stack = new Stack<Integer>();
 
-        for (var asteroid : asteroids) {
-            if (asteroid > 0)
-                stack.push(asteroid);
+        for (var i = 0; i < n; i++) {
+            var num = asteroids[i];
+
+            if (num > 0)
+                stack.push(num);
 
             else {
-                while (!stack.isEmpty() && stack.peek() > 0 && stack.peek() < Math.abs(asteroid))
+                while (!stack.isEmpty() && stack.peek() > 0 && stack.peek() < Math.abs(num))
                     stack.pop();
 
-                if (!stack.isEmpty() && stack.peek() == Math.abs(asteroid))
+                if (!stack.isEmpty() && stack.peek() == Math.abs(num))
                     stack.pop();
 
                 else if (stack.isEmpty() || stack.peek() < 0)
-                    stack.push(asteroid);
+                    stack.push(num);
             }
         }
 
-        var index = 0;
         int[] ans = new int[stack.size()];
-        for (var num : stack)
-            ans[index++] = num;
+        for (var i = 0; i < stack.size(); i++)
+            ans[i] = stack.get(i);
 
         return ans;
     }
