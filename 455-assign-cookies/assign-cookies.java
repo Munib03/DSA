018f636c8ff54d7fb530e5fd00e7b1e8fd5116b6
Hedayatullah Yamin <67416501+Munib03@users.noreply.github.com>
2025-08-13 14:36:48 +0430

    Time: 11 ms (35.59%) | Memory: 49.1 MB (9.18%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        var n = g.length;
+        var m = s.length;
+
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        var cnt = 0;
+        var left = 0;
+        var right = 0;
+
+        while (left < n && right < m) {
+            var greedFactor = g[left];
+            var cookieSize = s[right];
+
+            if (cookieSize >= greedFactor) {
+                cnt++;
+                left++;
+                right++;
+            }
+
+            else
+                right++;
+        }
+
+        return cnt;
+    }
+}
