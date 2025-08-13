4864e84367da6366652caefe09419ca7b45c2718
Hedayatullah Yamin <67416501+Munib03@users.noreply.github.com>
2025-08-13 18:45:38 +0430

    Time: 1 ms (99.54%) | Memory: 55.7 MB (85.88%) - LeetSync

---
+++ 890-lemonade-change/lemonade-change.java
@@ -0,0 +1,34 @@
+class Solution {
+public boolean lemonadeChange(int[] bills) {
+  var fives = 0;
+  var tens = 0;
+  
+  for (var bill: bills) {
+    if (bill == 5)
+      fives++;
+    
+    else if (bill == 10) {
+      if (fives <= 0)
+        return false;
+      
+      fives--;
+      tens++;
+    }
+    
+    else {
+      if (tens > 0 && fives > 0) {
+        tens--;
+        fives--;
+      }
+      
+      else if (fives >= 3)
+        fives -= 3;
+      
+      else 
+        return false;
+    }
+  }
+  
+  return true;
+}
+}
