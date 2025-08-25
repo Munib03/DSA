6c4fa0c4d55f0201661a7ee797b9e0741dfc3360
Hedayatullah Yamin <67416501+Munib03@users.noreply.github.com>
2025-08-25 17:40:18 +0430

    Time: 1 ms (54.74%) | Memory: 40.9 MB (84.21%) - LeetSync

---
+++ 1026-string-without-aaa-or-bbb/string-without-aaa-or-bbb.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String strWithout3a3b(int a, int b) {
+        StringBuilder res = new StringBuilder();
+
+        while (a > 0 || b > 0) {
+            int len = res.length();
+
+            if (len >= 2 && res.charAt(len - 1) == res.charAt(len - 2)) {
+                if (res.charAt(len - 1) == 'a') {
+                    res.append('b');
+                    b--;
+                } else {
+                    res.append('a');
+                    a--;
+                }
+            } else {
+                if (a >= b) {
+                    res.append('a');
+                    a--;
+                } else {
+                    res.append('b');
+                    b--;
+                }
+            }
+        }
+
+        return res.toString();
+    }
+
+}
