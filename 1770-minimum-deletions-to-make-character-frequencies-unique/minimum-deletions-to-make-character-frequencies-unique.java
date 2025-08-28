a723c30a1fcada951733f0c84a1ca217c07a3b9d
Hedayatullah Yamin <67416501+Munib03@users.noreply.github.com>
2025-08-28 16:22:58 +0430

    Time: 52 ms (29.54%) | Memory: 45.6 MB (33.73%) - LeetSync

--- 1770-minimum-deletions-to-make-character-frequencies-unique/minimum-deletions-to-make-character-frequencies-unique.java
+++ 1770-minimum-deletions-to-make-character-frequencies-unique/minimum-deletions-to-make-character-frequencies-unique.java
@@ -1,6 +1,6 @@
 class Solution {
     public int minDeletions(String s) {
-        var map = sortByValueDesc(addToMap(s));
+        var map = addToMap(s);
 
         var set = new HashSet<Integer>();
         var cnt = 0;
@@ -24,18 +24,5 @@ class Solution {
             map.put(ch, map.getOrDefault(ch, 0) + 1);
 
         return map;
-    }
-
-    private Map<Character, Integer> sortByValueDesc(Map<Character, Integer> map) {
-        List<Map.Entry<Character, Integer>> list = new ArrayList<>(map.entrySet());
-
-        list.sort((e1, e2) -> e2.getValue().compareTo(e1.getValue()));
-
-        Map<Character, Integer> sortedMap = new LinkedHashMap<>();
-        for (Map.Entry<Character, Integer> entry : list) {
-            sortedMap.put(entry.getKey(), entry.getValue());
-        }
-
-        return sortedMap;
     }
 }
