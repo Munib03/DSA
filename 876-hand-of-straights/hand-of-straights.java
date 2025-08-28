6a6fc47fb3acf2b369a735532c4cb6145cc7426c
Hedayatullah Yamin <67416501+Munib03@users.noreply.github.com>
2025-08-28 08:40:30 +0430

    Time: 33 ms (68.45%) | Memory: 45.6 MB (54.96%) - LeetSync

---
+++ 876-hand-of-straights/hand-of-straights.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean isNStraightHand(int[] hand, int groupSize) {
+        var n = hand.length;
+        if (n % groupSize != 0)
+            return false;
+
+        var count = new HashMap<Integer, Integer>();
+        for (var num : hand)
+            count.put(num, count.getOrDefault(num, 0) + 1);
+
+        var minH = new PriorityQueue<>(count.keySet());
+
+        while (!minH.isEmpty()) {
+            var peak = minH.peek();
+
+            for (int i = peak; i < peak + groupSize; i++) {
+                if (!count.containsKey(i))
+                    return false;
+
+                count.put(i, count.get(i) - 1);
+
+                if (count.get(i) == 0) {
+                    if (i != minH.peek())
+                        return false;
+
+                    minH.poll();
+                }
+            }
+        }
+        return true;
+    }
+}
