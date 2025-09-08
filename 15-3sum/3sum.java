d530ff2ab44424f743ca21a93e80b490f4b6a733
Hedayatullah Yamin <67416501+Munib03@users.noreply.github.com>
2025-09-08 08:53:00 +0430

    Time: 29 ms (74.51%) | Memory: 53 MB (11.68%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,41 +1,46 @@
 class Solution {
-// O(n log n) time, space O(1)
-public List<List<Integer>> threeSum(int[] nums) {
-  var n = nums.length;
+    public List<List<Integer>> threeSum(int[] nums) {
+        var n = nums.length;
 
-  Arrays.sort(nums);
-  Set<List<Integer>> ans = new HashSet<>();
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
 
-  for (var i = 0; i < n; i++) {
-    var currElement = nums[i];
+        for (var i = 0; i < n; i++) {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
 
-    var left = i + 1;
-    var right = n - 1;
+            var currElement = nums[i];
+            var left = i + 1;
+            var right = n - 1;
 
+            while (left < n && left < right) {
+                var leftNum = nums[left];
+                var rightNum = nums[right];
 
-    while (left < n && left < right) {
-      var leftNum = nums[left];
-      var rightNum = nums[right];
+                var sum = currElement + leftNum + rightNum;
+                if (sum == 0) {
+                    var list = new ArrayList<Integer>();
+                    list.add(currElement);
+                    list.add(leftNum);
+                    list.add(rightNum);
 
-      var sum = currElement + leftNum + rightNum;
-      if (sum == 0) {
-        var list = new ArrayList<Integer>();
-        list.add(currElement);
-        list.add(leftNum);
-        list.add(rightNum);
+                    ans.add(list);
+                    left++;
+                    right--;
 
-        ans.add(list);
-        left++;
-        right--;
-      }
-      else if (sum < 0)
-        left++;
+                    while (left < right && nums[left] == nums[left - 1])
+                        left++;
 
-      else
-        right--;
+                    while (left < right && nums[right] == nums[right + 1])
+                        right--;
+                } else if (sum < 0)
+                    left++;
+
+                else
+                    right--;
+            }
+        }
+
+        return ans;
     }
-  }
-
-  return new ArrayList<>(ans);
-}
 }
