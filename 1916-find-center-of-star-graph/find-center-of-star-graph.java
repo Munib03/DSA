e367db4f819ae3629492d9281e899c9bbd8be345
Hedayatullah Yamin <67416501+Munib03@users.noreply.github.com>
2025-09-10 17:49:34 +0430

    Time: 0 ms (100.00%) | Memory: 74.4 MB (24.64%) - LeetSync

--- 1916-find-center-of-star-graph/find-center-of-star-graph.java
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.java
@@ -1,25 +1,12 @@
 class Solution {
     public int findCenter(int[][] edges) {
-        var n = edges.length;
-
         var first = edges[0][0];
         var second = edges[0][1];
 
-        var cnt1 = 0;
-        var cnt2 = 0;
+        var currFirst = edges[1][0];
+        var currSecond = edges[1][1];
 
-        for (var i = 1; i < n; i++) {
-            var currFirst = edges[i][0];
-            var currSecond = edges[i][0];
-
-            if (currFirst == first || currSecond == first)
-                cnt1++;
-
-            else if (currFirst == second || currSecond == second)
-                cnt2++;
-        }
-
-        if (cnt1 > cnt2)
+        if (currFirst == first || currSecond == first)
             return first;
 
         return second;
