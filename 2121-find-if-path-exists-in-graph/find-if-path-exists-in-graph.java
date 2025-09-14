766d062d781939074729358c2824940219ea7365
Hedayatullah Yamin <67416501+Munib03@users.noreply.github.com>
2025-09-14 13:35:58 +0430

    Time: 62 ms (83.96%) | Memory: 145.9 MB (69.78%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -1,35 +1,35 @@
 class Solution {
     public boolean validPath(int n, int[][] edges, int source, int destination) {
-        if (source == destination) return true;
-        List<List<Integer>> adjList = new ArrayList<>();
+        if (source == destination)
+            return true;
 
-        for (int i = 0; i < n; i++) {
-            adjList.add(new ArrayList<>());
-        }
+        List<List<Integer>> graph = new ArrayList<>();
 
-        for (int[] edge : edges) {
-            adjList.get(edge[0]).add(edge[1]);
-            adjList.get(edge[1]).add(edge[0]);
+        for (var i = 0; i < n; i++)
+            graph.add(new ArrayList<>());
+
+        for (var edge : edges) {
+            graph.get(edge[0]).add(edge[1]);
+            graph.get(edge[1]).add(edge[0]);
         }
 
         boolean[] visited = new boolean[n];
+        var queue = new ArrayDeque<Integer>();
 
-        Queue<Integer> queue = new ArrayDeque<>();
-        queue.offer(source);
         visited[source] = true;
+        queue.offer(source);
 
         while (!queue.isEmpty()) {
-            int currSource = queue.poll();
+            var frontOfQueue = queue.poll();
 
-            if (currSource == destination)
+            if (frontOfQueue == destination)
                 return true;
 
-            for (int neighbor : adjList.get(currSource)) {
+            for (var neighbor : graph.get(frontOfQueue))
                 if (!visited[neighbor]) {
-                    visited[neighbor] = true;
                     queue.offer(neighbor);
+                    visited[neighbor] = true;
                 }
-            }
         }
 
         return false;
