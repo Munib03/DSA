4901a9662f58c8583931ba3baf3d7becd576bef1
Hedayatullah Yamin <67416501+Munib03@users.noreply.github.com>
2025-09-20 14:10:21 +0430

    Time: 4 ms (49.25%) | Memory: 50.2 MB (29.11%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,40 +1,33 @@
 class Solution {
+
     public int numIslands(char[][] grid) {
-        var rows = grid.length;
-        var cols = grid[0].length;
+        var n = grid.length;
+        var m = grid[0].length;
 
-        if (rows == 0)
-            return 0;
-
-        boolean[][] visitedNodes = new boolean[rows][cols];
+        boolean[][] visitedNodes = new boolean[n][m];
         var cnt = 0;
 
-        for (var i = 0; i < rows; i++) {
-            for (var j = 0; j < cols; j++) {
-
+        for (var i = 0; i < n; i++) {
+            for (var j = 0; j < m; j++) {
                 if (grid[i][j] == '1' && !visitedNodes[i][j]) {
                     dfs(grid, i, j, visitedNodes);
                     cnt++;
                 }
-
             }
         }
 
         return cnt;
     }
 
-    private void dfs(char[][] grid, int row, int col, boolean[][] visited) {
-        if (row < 0 || col < 0 || row >= grid.length || col >= grid[0].length)
+    private void dfs(char[][] grid, int row, int col, boolean[][] visitedNodes) {
+        if (row < 0 || col < 0 || row >= grid.length || col >= grid[0].length || visitedNodes[row][col]
+                || grid[row][col] == '0')
             return;
 
-        if (visited[row][col] || grid[row][col] == '0')
-            return;
-
-        visited[row][col] = true;
-
-        dfs(grid, row + 1, col, visited);
-        dfs(grid, row - 1, col, visited);
-        dfs(grid, row, col + 1, visited);
-        dfs(grid, row, col - 1, visited);
+        visitedNodes[row][col] = true;
+        dfs(grid, row + 1, col, visitedNodes);
+        dfs(grid, row - 1, col, visitedNodes);
+        dfs(grid, row, col + 1, visitedNodes);
+        dfs(grid, row, col - 1, visitedNodes);
     }
 }
