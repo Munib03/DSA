928fbc1545d6c1abd41b26b3ed82d3ce131eb827
Hedayatullah Yamin <67416501+Munib03@users.noreply.github.com>
2025-09-22 20:43:11 +0430

    Time: 21 ms (6.13%) | Memory: 45.6 MB (12.93%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.java
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -1,46 +1,84 @@
 class Solution {
+    class Graph {
+        private class Node {
+            private final String label;
+
+            private Node(String label) {
+                this.label = label;
+            }
+
+            @Override
+            public String toString() {
+                return label;
+            }
+        }
+
+        private final Map<String, Node> nodes = new HashMap<>();
+        private final Map<Node, List<Node>> adjacencyList = new HashMap<>();
+
+        public void addNode(String label) {
+            var newNode = new Node(label);
+
+            nodes.putIfAbsent(label, newNode);
+            adjacencyList.putIfAbsent(newNode, new ArrayList<>());
+        }
+
+        public void addEdge(String from, String to) {
+            var fromNode = nodes.get(from);
+            var toNode = nodes.get(to);
+
+            if (fromNode == null || toNode == null)
+                return;
+
+            adjacencyList.get(fromNode).add(toNode);
+        }
+
+        public boolean isBipartiteGraph() {
+            var map = new HashMap<String, Integer>();
+
+            for (var node : nodes.values()) {
+                if (!map.containsKey(node.label)) {
+                    if (!dfs(node, map, 0))
+                        return false;
+                }
+            }
+
+            return true;
+        }
+
+        // Inner Details of the class
+        private boolean dfs(Node root, Map<String, Integer> map, int color) {
+            map.putIfAbsent(root.label, color);
+
+            for (var neighbor : adjacencyList.get(root)) {
+                if (map.containsKey(neighbor.label)) {
+                    if (map.get(neighbor.label) == color)
+                        return false;
+                } else {
+                    var res = dfs(neighbor, map, 1 - color);
+                    if (!res)
+                        return false;
+                }
+            }
+
+            return true;
+        }
+    }
+
     public boolean isBipartite(int[][] graph) {
         var n = graph.length;
 
-        List<List<Integer>> list = new ArrayList<>();
+        var theRealGraph = new Graph();
         for (var i = 0; i < n; i++)
-            list.add(new ArrayList<>());
+            theRealGraph.addNode(i + "");
 
-        for (var i = 0; i < n; i++) {
-            var edges = graph[i];
-
-            for (var edge : edges)
-                list.get(i).add(edge);
-        }
-
-        var map = new HashMap<Integer, Integer>();
-        for (var i = 0; i < n; i++) {
-            if (!map.containsKey(i)) {
-                var res = dfs(i, list, map, 0);
-
-                if (!res)
-                    return false;
+        for (int i = 0; i < graph.length; i++) {
+            for (int neighbor : graph[i]) {
+                theRealGraph.addEdge(i + "", neighbor + "");
             }
         }
 
-        return true;
+        return theRealGraph.isBipartiteGraph();
     }
 
-    private boolean dfs(int node, List<List<Integer>> list, Map<Integer, Integer> map, int color) {
-        map.put(node, color);
-
-        for (var neighbor : list.get(node)) {
-            if (map.containsKey(neighbor)) {
-                if (map.get(neighbor) == color)
-                    return false;
-            } else {
-                var res = dfs(neighbor, list, map, 1 - color);
-                if (!res)
-                    return false;
-            }
-
-        }
-
-        return true;
-    }
 }
