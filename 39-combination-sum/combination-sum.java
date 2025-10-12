857ab712c8eb0d468bae45934923f0df75d4eafb
Hedayatullah Yamin <67416501+Munib03@users.noreply.github.com>
2025-10-12 15:34:29 +0430

    Time: 5 ms (6.95%) | Memory: 44.8 MB (31.82%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,26 +1,27 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        rec(0, candidates, target, new ArrayList<>());
+        rec(0, 0, target, candidates, new ArrayList<>());
 
         return new ArrayList<>(ansSet);
     }
 
     private final Set<List<Integer>> ansSet = new HashSet<>();
 
-    private void rec(int index, int[] nums, int target, List<Integer> list) {
-        if (index >= nums.length || target < 0)
+    private void rec(int index, int sum, int target, int[] nums, List<Integer> list) {
+        if (index >= nums.length || sum > target)
             return;
 
-        else if (target == 0) {
+        if (sum == target) {
             var temp = new ArrayList<>(list);
             ansSet.add(temp);
+
             return;
         }
 
         list.add(nums[index]);
-        rec(index, nums, target - nums[index], list);
+        rec(index, sum + nums[index], target, nums, list);
 
         list.remove((Integer) nums[index]);
-        rec(index + 1, nums, target, list);
+        rec(index + 1, sum, target, nums, list);
     }
 }
