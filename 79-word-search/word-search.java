60d0a674167058a1789fb523e799d7414a6d2a7f
Hedayatullah Yamin <67416501+Munib03@users.noreply.github.com>
2025-10-12 17:17:38 +0430

    Time: 1873 ms (5.01%) | Memory: 265.8 MB (5.39%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,44 +1,52 @@
 class Solution {
-    private final List<String> ansList = new ArrayList<>();
+private final List<String> ansList = new ArrayList<>();
 
-    public boolean exist(char[][] board, String word) {
-        var n = board.length;
-        var m = board[0].length;
+public boolean exist(char[][] board, String word) {
+  int n = board.length, m = board[0].length;
 
-        for (var i = 0; i < n; i++) {
-            for (var j = 0; j < m; j++) {
-                dfs(board, new boolean[n][m], i, j, new StringBuilder(), word);
-            }
-        }
-
-        for (var s : ansList)
-            if (s.equals(word))
-                return true;
-
-        return false;
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      if (board[i][j] == word.charAt(0)) {
+        dfs(board, new boolean[n][m], i, j, new StringBuilder(), word);
+        
+        for (String s : ansList)
+          if (s.equals(word))
+            return true;
+      }
     }
+  }
 
-    private void dfs(char[][] board, boolean[][] visited, int row, int col, StringBuilder sb, String word) {
-        if (row < 0 || row >= board.length || col < 0 || col >= board[0].length || visited[row][col])
-            return;
 
-        sb.append(board[row][col]);
+  return false;
+}
 
-        if (!word.startsWith(sb.toString())) {
-            sb.deleteCharAt(sb.length() - 1);
-            return;
-        }
+private void dfs(char[][] board, boolean[][] visited, int row, int col, StringBuilder sb, String word) {
+  if (row < 0 || row >= board.length || col < 0 || col >= board[0].length || visited[row][col])
+    return;
 
-        ansList.add(sb.toString());
+  sb.append(board[row][col]);
 
-        visited[row][col] = true;
+  if (!word.startsWith(sb.toString())) {
+    sb.deleteCharAt(sb.length() - 1);
+    return;
+  }
 
-        dfs(board, visited, row + 1, col, sb, word);
-        dfs(board, visited, row - 1, col, sb, word);
-        dfs(board, visited, row, col + 1, sb, word);
-        dfs(board, visited, row, col - 1, sb, word);
+  ansList.add(sb.toString());
 
-        visited[row][col] = false;
-        sb.deleteCharAt(sb.length() - 1);
-    }
+  if (sb.length() == word.length()) {
+    sb.deleteCharAt(sb.length() - 1);
+    return;
+  }
+
+  visited[row][col] = true;
+
+  dfs(board, visited, row + 1, col, sb, word);
+  dfs(board, visited, row - 1, col, sb, word);
+  dfs(board, visited, row, col + 1, sb, word);
+  dfs(board, visited, row, col - 1, sb, word);
+
+  visited[row][col] = false;
+  sb.deleteCharAt(sb.length() - 1);
+}
+
 }
