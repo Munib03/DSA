72c6d959f27ef8c79082eb47663e57e38309eb9a
Hedayatullah Yamin <67416501+Munib03@users.noreply.github.com>
2025-10-13 09:23:54 +0430

    Time: 3 ms (18.58%) | Memory: 43.6 MB (62.70%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -1,25 +1,23 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         Arrays.sort(nums);
+        recursion(0, 0, nums, new ArrayList<>());
 
-        List<List<Integer>> ans = new ArrayList<>();
-
-        backtrack(nums, 0, new ArrayList<>(), ans);
-
-        return ans;
+        return new ArrayList<>(ansSet);
     }
 
-    private void backtrack(int[] nums, int startIndex, List<Integer> path, List<List<Integer>> res) {
-        res.add(new ArrayList<>(path));
+    private final Set<List<Integer>> ansSet = new HashSet<>();
 
-        for (var i = startIndex; i < nums.length; i++) {
-            if (i > startIndex && nums[i] == nums[i - 1])
-                continue;
-
-            path.add(nums[i]);
-
-            backtrack(nums, i + 1, path, res);
-            path.removeLast();
+    private void recursion(int index, int sum, int[] nums, List<Integer> list) {
+        if (index >= nums.length) {
+            ansSet.add(new ArrayList<>(list));
+            return;
         }
+
+        list.add(nums[index]);
+        recursion(index + 1, sum + nums[index], nums, list);
+
+        list.removeLast();
+        recursion(index + 1, sum, nums, list);
     }
 }
