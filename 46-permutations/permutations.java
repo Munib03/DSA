b292c1391a9669deda28e3f310000251b16f7bb2
Hedayatullah Yamin <67416501+Munib03@users.noreply.github.com>
2025-10-14 10:33:35 +0430

    Time: 2 ms (34.33%) | Memory: 44.6 MB (46.64%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,27 +1,28 @@
 class Solution {
-public List<List<Integer>> permute(int[] nums) {
-  rec(0, nums, new ArrayList<>(), new HashSet<>());
-  
-  return ansList;
-}
+    public List<List<Integer>> permute(int[] nums) {
+        rec(nums, new ArrayList<>(), new HashSet<>());
 
-private final List<List<Integer>> ansList = new ArrayList<>();
-private void rec(int index, int[] nums, List<Integer> list, Set<Integer> set) {
-  if (list.size() >= nums.length) {
-    ansList.add(new ArrayList<>(list));
-    return;
-  }
-  
-  for (var i=0; i < nums.length; i++) {
-    if (!set.contains(i)) {
-      set.add(i);
-      list.add(nums[i]);
-
-      rec(i, nums, list, set);
-      
-      list.removeLast();
-      set.remove(i);
+        return ansList;
     }
-  }
-}
+
+    private final List<List<Integer>> ansList = new ArrayList<>();
+
+    private void rec(int[] nums, List<Integer> list, Set<Integer> set) {
+        if (list.size() >= nums.length) {
+            ansList.add(new ArrayList<>(list));
+            return;
+        }
+
+        for (var i = 0; i < nums.length; i++) {
+            if (!set.contains(i)) {
+                set.add(i);
+                list.add(nums[i]);
+
+                rec(nums, list, set);
+
+                list.removeLast();
+                set.remove(i);
+            }
+        }
+    }
 }
