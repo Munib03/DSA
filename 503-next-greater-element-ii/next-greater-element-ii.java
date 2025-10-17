49b5489949e71b5ff850039a783df328c1396ce2
Hedayatullah Yamin <67416501+Munib03@users.noreply.github.com>
2025-10-17 08:54:22 +0430

    Time: 18 ms (28.99%) | Memory: 46.6 MB (29.07%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.java
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -2,23 +2,19 @@ class Solution {
     public int[] nextGreaterElements(int[] nums) {
         var n = nums.length;
 
-        int[] ans = new int[2 * n];
-        for (var i = 0; i < 2 * n; i++)
-            ans[i] = nums[i % n];
-
         var stack = new Stack<Integer>();
-        int[] theAns = new int[2 * n];
+        int[] theAns = new int[n];
 
         for (var i = 2 * n - 1; i >= 0; i--) {
-            var currNum = ans[i];
+            var currNum = nums[i % n];
 
             while (!stack.isEmpty() && currNum >= stack.peek())
                 stack.pop();
 
-            theAns[i] = stack.isEmpty() ? -1 : stack.peek();
+            theAns[i % n] = stack.isEmpty() ? -1 : stack.peek();
             stack.push(currNum);
         }
 
-        return Arrays.copyOf(theAns, n);
+        return theAns;
     }
 }
