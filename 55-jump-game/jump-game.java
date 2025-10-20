1406572bbb44e12b921db64f2d74691ebd1be871
Hedayatullah Yamin <67416501+Munib03@users.noreply.github.com>
2025-10-20 11:09:55 +0430

    Time: 3 ms (38.86%) | Memory: 46 MB (32.00%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,20 +1,18 @@
 class Solution {
-public boolean canJump(int[] nums) {
-  var n = nums.length;
+    public boolean canJump(int[] nums) {
+        var n = nums.length;
 
-  var max = 0;
+        var maxIndex = 0;
 
-  for (var i=0; i < n; i++) {
-    var num = nums[i];
+        for (var i = 0; i < n; i++) {
+            var num = nums[i];
 
-    if (i == max && num == 0 && i != n - 1)
-      return false;
+            if (i > maxIndex)
+                return false;
 
-    max = Math.max(max, i + num);
-    if (max >= n - 1)
-      return true;
-  }
+            maxIndex = Math.max(maxIndex, num + i);
+        }
 
-  return true;
-}
+        return true;
+    }
 }
