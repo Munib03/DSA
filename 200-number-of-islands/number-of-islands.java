edfdafa2b307f7449ae50edd1eda460831683353
Hedayatullah Yamin <67416501+Munib03@users.noreply.github.com>
2025-10-21 20:29:12 +0430

    Time: 3 ms (84.48%) | Memory: 49.7 MB (54.12%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -3,13 +3,13 @@ class Solution {
         var n = grid.length;
         var m = grid[0].length;
 
-        boolean[][] visitedNodes = new boolean[n][m];
+        boolean[][] visited = new boolean[n][m];
         var cnt = 0;
 
         for (var i = 0; i < n; i++) {
             for (var j = 0; j < m; j++) {
-                if (grid[i][j] == '1' && !visitedNodes[i][j]) {
-                    bfs(grid, i, j, visitedNodes);
+                if (grid[i][j] == '1' && !visited[i][j]) {
+                    dfs(grid, i, j, visited);
                     cnt++;
                 }
             }
@@ -18,38 +18,17 @@ class Solution {
         return cnt;
     }
 
-    private void bfs(char[][] grid, int row, int col, boolean[][] visited) {
-        var n = grid.length;
-        var m = grid[0].length;
-
-        int[][] directions = {
-                { 1, 0 },
-                { 0, 1 },
-                { -1, 0 },
-                { 0, -1 }
-        };
+    private void dfs(char[][] grid, int row, int col, boolean[][] visited) {
+        if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length || visited[row][col]
+                || grid[row][col] == '0')
+            return;
 
         visited[row][col] = true;
 
-        var queue = new LinkedList<int[]>();
-        queue.offer(new int[] { row, col });
-
-        while (!queue.isEmpty()) {
-            var frontOfQueue = queue.poll();
-
-            var r = frontOfQueue[0];
-            var c = frontOfQueue[1];
-
-            for (var direction : directions) {
-                var nr = r + direction[0];
-                var nc = c + direction[1];
-
-                if (nr >= 0 && nr < n && nc >= 0 && nc < m && grid[nr][nc] == '1' && !visited[nr][nc]) {
-                    queue.offer(new int[] { nr, nc });
-                    visited[nr][nc] = true;
-                }
-            }
-        }
+        dfs(grid, row + 1, col, visited);
+        dfs(grid, row - 1, col, visited);
+        dfs(grid, row, col + 1, visited);
+        dfs(grid, row, col - 1, visited);
     }
 
 }
