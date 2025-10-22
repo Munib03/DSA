ab1ad78358fdaa193acae29fd3487bb6ead88c30
Hedayatullah Yamin <67416501+Munib03@users.noreply.github.com>
2025-10-22 11:43:09 +0430

    Time: 3 ms (30.67%) | Memory: 45.2 MB (86.15%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -3,24 +3,58 @@ class Solution {
         var n = board.length;
         var m = board[0].length;
 
+        var queue = new LinkedList<int[]>();
         boolean[][] visited = new boolean[n][m];
 
         // This for first row and last row
         for (var i = 0; i < m; i++) {
-            if (!visited[0][i] && board[0][i] == 'O')
-                dfs(board, 0, i, visited);
+            if (!visited[0][i] && board[0][i] == 'O') {
+                queue.offer(new int[] { 0, i });
+                visited[0][i] = true;
+                board[0][i] = 'M';
+            }
 
-            if (!visited[n - 1][i] && board[n - 1][i] == 'O')
-                dfs(board, n - 1, i, visited);
+            if (!visited[n - 1][i] && board[n - 1][i] == 'O') {
+                queue.offer(new int[] { n - 1, i });
+                visited[n - 1][i] = true;
+                board[n - 1][i] = 'M';
+            }
         }
 
         // This is for first and last column
         for (var i = 0; i < n; i++) {
-            if (!visited[i][0] && board[i][0] == 'O')
-                dfs(board, i, 0, visited);
+            if (!visited[i][0] && board[i][0] == 'O') {
+                queue.offer(new int[] { i, 0 });
+                visited[i][0] = true;
+                board[i][0] = 'M';
+            }
 
-            if (!visited[i][m - 1] && board[i][m - 1] == 'O')
-                dfs(board, i, m - 1, visited);
+            if (!visited[i][m - 1] && board[i][m - 1] == 'O') {
+                queue.offer(new int[] { i, m - 1 });
+                visited[i][m - 1] = true;
+                board[i][m - 1] = 'M';
+            }
+        }
+
+        int[] rows = { -1, 0, 1, 0 };
+        int[] cols = { 0, 1, 0, -1 };
+
+        while (!queue.isEmpty()) {
+            var topOfQueue = queue.pop();
+            var row = topOfQueue[0];
+            var col = topOfQueue[1];
+
+            for (var i = 0; i < rows.length; i++) {
+                var newRow = row + rows[i];
+                var newCol = col + cols[i];
+
+                if (newRow >= 0 && newRow < n && newCol >= 0 && newCol < m && !visited[newRow][newCol]
+                        && board[newRow][newCol] == 'O') {
+                    queue.offer(new int[] { newRow, newCol });
+                    visited[newRow][newCol] = true;
+                    board[newRow][newCol] = 'M';
+                }
+            }
         }
 
         for (var i = 0; i < n; i++) {
@@ -33,22 +67,6 @@ class Solution {
                     board[i][j] = 'X';
             }
         }
-
-    }
-
-    private void dfs(char[][] board, int row, int col, boolean[][] visited) {
-        if (row < 0 || row >= board.length || col < 0 || col >= board[row].length
-                || visited[row][col] || board[row][col] != 'O')
-            return;
-
-        visited[row][col] = true;
-        board[row][col] = 'M';
-
-        dfs(board, row + 1, col, visited);
-        dfs(board, row - 1, col, visited);
-        dfs(board, row, col + 1, visited);
-        dfs(board, row, col - 1, visited);
-
     }
 
 }
