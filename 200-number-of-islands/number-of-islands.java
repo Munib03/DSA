54cce779f6e63935e7e196e63f73d0e2b176742f
Hedayatullah Yamin <67416501+Munib03@users.noreply.github.com>
2025-10-22 12:03:19 +0430

    Time: 7 ms (16.17%) | Memory: 48.7 MB (97.45%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -3,32 +3,43 @@ class Solution {
         var n = grid.length;
         var m = grid[0].length;
 
-        boolean[][] visited = new boolean[n][m];
+        int[] rows = { -1, 0, 1, 0 };
+        int[] cols = { 0, 1, 0, -1 };
+
         var cnt = 0;
+        var queue = new LinkedList<int[]>();
 
         for (var i = 0; i < n; i++) {
             for (var j = 0; j < m; j++) {
-                if (grid[i][j] == '1' && !visited[i][j]) {
-                    dfs(grid, i, j, visited);
+                var curr = grid[i][j];
+
+                if (curr == '1') {
+                    grid[i][j] = '0';
+                    queue.offer(new int[] { i, j });
+
+                    while (!queue.isEmpty()) {
+                        var topOfQueue = queue.pop();
+
+                        var row = topOfQueue[0];
+                        var col = topOfQueue[1];
+
+                        for (var o = 0; o < rows.length; o++) {
+                            var newRow = row + rows[o];
+                            var newCol = col + cols[o];
+
+                            if (newRow >= 0 && newRow < n && newCol >= 0 && newCol < m && grid[newRow][newCol] == '1') {
+                                queue.offer(new int[] { newRow, newCol });
+                                grid[newRow][newCol] = '0';
+                            }
+                        }
+                    }
+
                     cnt++;
                 }
             }
         }
 
         return cnt;
-    }
-
-    private void dfs(char[][] grid, int row, int col, boolean[][] visited) {
-        if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length || visited[row][col]
-                || grid[row][col] == '0')
-            return;
-
-        visited[row][col] = true;
-
-        dfs(grid, row + 1, col, visited);
-        dfs(grid, row - 1, col, visited);
-        dfs(grid, row, col + 1, visited);
-        dfs(grid, row, col - 1, visited);
     }
 
 }
