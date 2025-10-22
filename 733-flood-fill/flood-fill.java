0d7ccf09d5528c91c6847614820ca1de676cdba9
Hedayatullah Yamin <67416501+Munib03@users.noreply.github.com>
2025-10-22 08:37:57 +0430

    Time: 0 ms (100.00%) | Memory: 45 MB (52.07%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -7,35 +7,25 @@ class Solution {
         if (initialColor == color)
             return image;
 
-        var queue = new LinkedList<int[]>();
-
-        queue.offer(new int[] { sr, sc });
-        image[sr][sc] = color;
-
-        int[][] directions = {
-                { 1, 0 },
-                { 0, 1 },
-                { -1, 0 },
-                { 0, -1 }
-        };
-
-        while (!queue.isEmpty()) {
-            var frontOfQueue = queue.poll();
-
-            var row = frontOfQueue[0];
-            var col = frontOfQueue[1];
-
-            for (var direction : directions) {
-                var nr = row + direction[0];
-                var nc = col + direction[1];
-
-                if (nr >= 0 && nr < n && nc >= 0 && nc < m && image[nr][nc] == initialColor) {
-                    image[nr][nc] = color;
-                    queue.offer(new int[] { nr, nc });
-                }
-            }
-        }
+        dfs(image, sr, sc, color, initialColor, new boolean[n][m]);
 
         return image;
     }
+
+    private void dfs(int[][] image, int row, int col, int color, int initialColor, boolean[][] visited) {
+        if (row < 0 || row >= image.length || col < 0 || col >= image[row].length || image[row][col] != initialColor
+                || visited[row][col])
+            return;
+
+        visited[row][col] = true;
+        image[row][col] = color;
+
+        dfs(image, row + 1, col, color, initialColor, visited);
+        dfs(image, row - 1, col, color, initialColor, visited);
+        dfs(image, row, col + 1, color, initialColor, visited);
+        dfs(image, row, col - 1, color, initialColor, visited);
+
+        visited[row][col] = false;
+    }
+
 }
