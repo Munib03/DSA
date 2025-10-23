01777b872458d50289a9c9755899642634d67c04
Hedayatullah Yamin <67416501+Munib03@users.noreply.github.com>
2025-10-23 08:51:48 +0430

    Time: 117 ms (5.05%) | Memory: 55.7 MB (26.84%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.java
+++ 820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -1,31 +1,26 @@
 class Solution {
-    private class Graph {
-        private class Node {
-            private int label;
+    static class UnWeightedGraph {
+        private static class Node {
+            private final int label;
 
             private Node(int label) {
                 this.label = label;
             }
-
-            @Override
-            public String toString() {
-                return label + "";
-            }
         }
 
-        private final Map<Integer, Node> nodes = new HashMap<>();
+        private final Map<Integer, Node> nodesMap = new HashMap<>();
         private final Map<Node, List<Node>> adjacencyList = new HashMap<>();
 
-        public void addNode(int node) {
-            var newNode = new Node(node);
+        public void addNode(int val) {
+            var newNode = new Node(val);
 
-            nodes.putIfAbsent(node, newNode);
+            nodesMap.putIfAbsent(val, newNode);
             adjacencyList.putIfAbsent(newNode, new ArrayList<>());
         }
 
         public void addEdge(int from, int to) {
-            var fromNode = nodes.get(from);
-            var toNode = nodes.get(to);
+            var fromNode = nodesMap.get(from);
+            var toNode = nodesMap.get(to);
 
             if (fromNode == null || toNode == null)
                 return;
@@ -33,55 +28,56 @@ class Solution {
             adjacencyList.get(fromNode).add(toNode);
         }
 
-        public List<Integer> findEventualStates() {
-            var visiting = new HashSet<Node>();
+        private Set<Integer> hasCycle() {
             var visited = new HashSet<Node>();
+            var visiting = new TreeSet<Integer>();
 
-            for (var node : nodes.values())
+            for (var node : nodesMap.values())
                 hasCycle(node, visiting, visited);
 
-            var list = new ArrayList<Integer>();
-            for (var node : nodes.values())
-                if (!visiting.contains(node))
-                    list.add(node.label);
-
-            return list;
+            return visiting;
         }
 
         // Inner Details of the class
-        private boolean hasCycle(Node node, Set<Node> visiting, Set<Node> visited) {
-            visiting.add(node);
+        private boolean hasCycle(Node node, Set<Integer> visiting, Set<Node> visited) {
+            visiting.add(node.label);
 
             for (var neighbor : adjacencyList.get(node)) {
                 if (visited.contains(neighbor))
                     continue;
 
-                else if (visiting.contains(neighbor)) {
+                else if (visiting.contains(neighbor.label))
                     return true;
-                }
 
                 if (hasCycle(neighbor, visiting, visited))
                     return true;
             }
 
-            visiting.remove(node);
             visited.add(node);
+            visiting.remove(node.label);
 
             return false;
         }
-
     }
 
     public List<Integer> eventualSafeNodes(int[][] graph) {
-        var theGraph = new Graph();
+        var n = graph.length;
 
-        for (var i = 0; i < graph.length; i++)
-            theGraph.addNode(i);
+        var theRealGraph = new UnWeightedGraph();
+        for (var i = 0; i < n; i++)
+            theRealGraph.addNode(i);
 
-        for (var i = 0; i < graph.length; i++)
+        for (var i = 0; i < n; i++)
             for (var j = 0; j < graph[i].length; j++)
-                theGraph.addEdge(i, graph[i][j]);
+                theRealGraph.addEdge(i, graph[i][j]);
 
-        return theGraph.findEventualStates();
+        var notSafeNodes = theRealGraph.hasCycle();
+        var list = new ArrayList<Integer>();
+        for (var i = 0; i < n; i++)
+            if (!notSafeNodes.contains(i))
+                list.add(i);
+
+        return list;
     }
+
 }
