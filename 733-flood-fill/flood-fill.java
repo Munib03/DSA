6fd651b4110273f745b827bb4353ab0fb85d7f69
Hedayatullah Yamin <67416501+Munib03@users.noreply.github.com>
2025-11-06 09:22:37 +0430

    Time: 0 ms (100.00%) | Memory: 47 MB (5.28%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,31 +1,25 @@
 class Solution {
-    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        var n = image.length;
-        var m = image[0].length;
+public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+  var initialColor = image[sr][sc];
+  if (initialColor == color)
+    return image;
 
-        var initialColor = image[sr][sc];
-        if (initialColor == color)
-            return image;
+  dfs(image, sr, sc, initialColor, color);
 
-        dfs(image, sr, sc, color, initialColor, new boolean[n][m]);
+  return image;
+}
 
-        return image;
-    }
+private void dfs(int[][] image, int row, int col, int initialColor, int color) {
+  if (row < 0 || row >= image.length || col < 0 || col >= image[row].length || image[row][col] != initialColor)
+    return;
 
-    private void dfs(int[][] image, int row, int col, int color, int initialColor, boolean[][] visited) {
-        if (row < 0 || row >= image.length || col < 0 || col >= image[row].length || image[row][col] != initialColor
-                || visited[row][col])
-            return;
+  image[row][col] = color;
 
-        visited[row][col] = true;
-        image[row][col] = color;
+  dfs(image, row + 1, col, initialColor, color);
+  dfs(image, row - 1, col, initialColor, color);
+  dfs(image, row, col + 1, initialColor, color);
+  dfs(image, row, col - 1, initialColor, color);
 
-        dfs(image, row + 1, col, color, initialColor, visited);
-        dfs(image, row - 1, col, color, initialColor, visited);
-        dfs(image, row, col + 1, color, initialColor, visited);
-        dfs(image, row, col - 1, color, initialColor, visited);
-
-        visited[row][col] = false;
-    }
+}
 
 }
