e3caa70a2a5a2cd91e2027cbb6a8b861a51b8f80
Hedayatullah Yamin <67416501+Munib03@users.noreply.github.com>
2025-11-07 19:24:50 +0430

    Time: 2 ms (67.17%) | Memory: 44.3 MB (5.18%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -3,18 +3,16 @@ class Solution {
         var n = grid.length;
         var m = grid[0].length;
 
-        var set = new HashSet<String>();
-        var queue = new LinkedList<Tripe>();
+        var queue = new LinkedList<Triple>();
         var freshOranges = 0;
 
         for (var i = 0; i < n; i++) {
             for (var j = 0; j < m; j++) {
                 var curr = grid[i][j];
 
-                if (curr == 2) {
-                    queue.offer(new Tripe(i, j, 0));
-                    set.add(i + ", " + j);
-                } else if (curr == 1)
+                if (curr == 2)
+                    queue.offer(new Triple(i, j, 0));
+                else if (curr == 1)
                     freshOranges++;
             }
         }
@@ -23,11 +21,10 @@ class Solution {
         int[] cols = { 0, 1, 0, -1 };
 
         var maxTime = 0;
-        var freshOrangesSeenSoFar = 0;
+        var freshOrangesSoFar = 0;
 
         while (!queue.isEmpty()) {
-            var topOfQueue = queue.pop();
-
+            var topOfQueue = queue.poll();
             var row = topOfQueue.row;
             var col = topOfQueue.col;
             var time = topOfQueue.time;
@@ -38,22 +35,29 @@ class Solution {
                 var newRow = row + rows[i];
                 var newCol = col + cols[i];
 
-                if (newRow >= 0 && newRow < n && newCol >= 0 && newCol < m && !set.contains(newRow + ", " + newCol)
-                        && grid[newRow][newCol] == 1) {
-                    queue.offer(new Tripe(newRow, newCol, time + 1));
-                    set.add(newRow + ", " + newCol);
-                    freshOrangesSeenSoFar++;
+                if (newRow >= 0 && newRow < n && newCol >= 0 && newCol < m && grid[newRow][newCol] == 1) {
+                    queue.offer(new Triple(newRow, newCol, time + 1));
+                    grid[newRow][newCol] = 0;
+                    freshOrangesSoFar++;
                 }
             }
-
         }
 
-        if (freshOranges != freshOrangesSeenSoFar)
+        if (freshOranges != freshOrangesSoFar)
             return -1;
 
         return maxTime;
     }
 
-    private record Tripe(int row, int col, int time) {
+    private class Triple {
+        private final int row;
+        private final int col;
+        private final int time;
+
+        private Triple(int row, int col, int time) {
+            this.row = row;
+            this.col = col;
+            this.time = time;
+        }
     }
 }
