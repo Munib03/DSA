1f46870876538f4cda72df2ffdd263dcc5f22b9f
Hedayatullah Yamin <67416501+Munib03@users.noreply.github.com>
2025-11-09 16:50:45 +0430

    Time: 3 ms (31.37%) | Memory: 47.8 MB (9.58%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -4,35 +4,28 @@ class Solution {
         var m = board[0].length;
 
         var queue = new LinkedList<int[]>();
-        boolean[][] visited = new boolean[n][m];
 
-        // This for first row and last row
         for (var i = 0; i < m; i++) {
-            if (!visited[0][i] && board[0][i] == 'O') {
+            if (board[0][i] == 'O') {
                 queue.offer(new int[] { 0, i });
-                visited[0][i] = true;
                 board[0][i] = 'M';
             }
 
-            if (!visited[n - 1][i] && board[n - 1][i] == 'O') {
+            if (board[n - 1][i] == 'O') {
                 queue.offer(new int[] { n - 1, i });
-                visited[n - 1][i] = true;
-                board[n - 1][i] = 'M';
+                board[n-1][i] = 'M';
             }
         }
 
-        // This is for first and last column
         for (var i = 0; i < n; i++) {
-            if (!visited[i][0] && board[i][0] == 'O') {
+            if (board[i][0] == 'O') {
                 queue.offer(new int[] { i, 0 });
-                visited[i][0] = true;
                 board[i][0] = 'M';
             }
 
-            if (!visited[i][m - 1] && board[i][m - 1] == 'O') {
+            if (board[i][m - 1] == 'O') {
                 queue.offer(new int[] { i, m - 1 });
-                visited[i][m - 1] = true;
-                board[i][m - 1] = 'M';
+                board[i][m-1] = 'M';
             }
         }
 
@@ -40,7 +33,7 @@ class Solution {
         int[] cols = { 0, 1, 0, -1 };
 
         while (!queue.isEmpty()) {
-            var topOfQueue = queue.pop();
+            var topOfQueue = queue.poll();
             var row = topOfQueue[0];
             var col = topOfQueue[1];
 
@@ -48,10 +41,8 @@ class Solution {
                 var newRow = row + rows[i];
                 var newCol = col + cols[i];
 
-                if (newRow >= 0 && newRow < n && newCol >= 0 && newCol < m && !visited[newRow][newCol]
-                        && board[newRow][newCol] == 'O') {
+                if (newRow >= 0 && newRow < n && newCol >= 0 && newCol < m && board[newRow][newCol] == 'O') {
                     queue.offer(new int[] { newRow, newCol });
-                    visited[newRow][newCol] = true;
                     board[newRow][newCol] = 'M';
                 }
             }
@@ -59,9 +50,7 @@ class Solution {
 
         for (var i = 0; i < n; i++) {
             for (var j = 0; j < m; j++) {
-                var curr = board[i][j];
-
-                if (curr == 'M')
+                if (board[i][j] == 'M')
                     board[i][j] = 'O';
                 else
                     board[i][j] = 'X';
