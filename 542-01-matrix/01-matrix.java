f27a59a44396d1832f3c452ababf335fccdb1984
Hedayatullah Yamin <67416501+Munib03@users.noreply.github.com>
2025-11-09 16:32:31 +0430

    Time: 115 ms (5.23%) | Memory: 56.8 MB (5.94%) - LeetSync

--- 542-01-matrix/01-matrix.java
+++ 542-01-matrix/01-matrix.java
@@ -1,51 +1,47 @@
 class Solution {
-public int[][] updateMatrix(int[][] mat) {
-  var n = mat.length;
-  var m = mat[0].length;
+    public int[][] updateMatrix(int[][] mat) {
+        var n = mat.length;
+        var m = mat[0].length;
 
-  int[][] ans = new int[n][m];
-  var queue = new LinkedList<Triple>();
-  var set = new HashSet<String>();
+        var set = new HashSet<String>();
+        var queue = new LinkedList<Tripe>();
+        for (var i = 0; i < n; i++) {
+            for (var j = 0; j < m; j++) {
+                if (mat[i][j] == 0) {
+                    queue.offer(new Tripe(i, j, 0));
+                    mat[i][j] = 0;
+                    set.add(i + ", " + j);
+                }
+            }
+        }
 
-  for (var i=0; i < n; i++) {
-    for (var j=0; j < m; j++) {
-      var curr = mat[i][j];
+        int[] rows = { -1, 0, 1, 0 };
+        int[] cols = { 0, 1, 0, -1 };
 
-      if (curr == 0) {
-        queue.offer(new Triple(i, j, 0));
-        set.add(i + ", " + j);
-        ans[i][j] = 0;
-      }
-      else
-        ans[i][j] = Integer.MAX_VALUE;
+        while (!queue.isEmpty()) {
+            var topOfQueue = queue.poll();
+            var row = topOfQueue.row;
+            var col = topOfQueue.col;
+            var numOfSteps = topOfQueue.numOfSteps;
+
+            for (var i = 0; i < rows.length; i++) {
+                var newRow = row + rows[i];
+                var newCol = col + cols[i];
+
+                var key = newRow + ", " + newCol;
+                if (newRow >= 0 && newRow < n && newCol >= 0 && newCol < m && !set.contains(key)) {
+                    queue.offer(new Tripe(newRow, newCol, numOfSteps + 1));
+                    set.add(key);
+                    mat[newRow][newCol] = numOfSteps + 1;
+                }
+            }
+        }
+
+        return mat;
     }
-  }
 
-  int[] rows = {-1, 0, 1, 0};
-  int[] cols = {0, 1, 0, -1};
+    private record Tripe(int row, int col, int numOfSteps) {
 
-  while (!queue.isEmpty()) {
-    var topOfQueue = queue.pop();
-    var row = topOfQueue.row;
-    var col = topOfQueue.col;
-    var numOfSteps = topOfQueue.numberOfSteps;
-
-    for (var i=0; i < rows.length; i++) {
-      var newRow = rows[i] + row;
-      var newCol = cols[i] + col;
-
-      if (newRow >= 0 && newRow < n && newCol >= 0 && newCol < m && !set.contains(newRow + ", " + newCol)) {
-        queue.offer(new Triple(newRow, newCol, numOfSteps + 1));
-        set.add(newRow + ", " + newCol);
-        ans[newRow][newCol] = numOfSteps + 1;
-      }
     }
-  }
 
-  return ans;
-}
-
-private record Triple(int row, int col, int numberOfSteps) {
-
-}
 }
