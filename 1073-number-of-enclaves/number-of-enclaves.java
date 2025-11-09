db8fb8ba690cd98316436439514d7e3921338b19
Hedayatullah Yamin <67416501+Munib03@users.noreply.github.com>
2025-11-09 17:01:10 +0430

    Time: 6 ms (99.11%) | Memory: 60.8 MB (18.49%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.java
+++ 1073-number-of-enclaves/number-of-enclaves.java
@@ -3,53 +3,24 @@ class Solution {
         var n = grid.length;
         var m = grid[0].length;
 
-        var queue = new LinkedList<int[]>();
-
         for (var i = 0; i < m; i++) {
-            if (grid[0][i] == 1) {
-                queue.offer(new int[] { 0, i });
-                grid[0][i] = 0;
-            }
+            if (grid[0][i] == 1)
+                dfs(0, i, grid);
 
-            if (grid[n - 1][i] == 1) {
-                queue.offer(new int[] { n - 1, i });
-                grid[n - 1][i] = 0;
-            }
+            if (grid[n - 1][i] == 1)
+                dfs(n - 1, i, grid);
         }
 
         for (var i = 0; i < n; i++) {
-            if (grid[i][0] == 1) {
-                queue.offer(new int[] { i, 0 });
-                grid[i][0] = 0;
-            }
+            if (grid[i][0] == 1)
+                dfs(i, 0, grid);
 
-            if (grid[i][m - 1] == 1) {
-                queue.offer(new int[] { i, m - 1 });
-                grid[i][m - 1] = 0;
-            }
-        }
-
-        int[] rows = { -1, 0, 1, 0 };
-        int[] cols = { 0, 1, 0, -1 };
-
-        while (!queue.isEmpty()) {
-            var topOfQueue = queue.poll();
-
-            var row = topOfQueue[0];
-            var col = topOfQueue[1];
-
-            for (var i = 0; i < rows.length; i++) {
-                var newRow = row + rows[i];
-                var newCol = col + cols[i];
-
-                if (newRow >= 0 && newRow < n && newCol >= 0 && newCol < m && grid[newRow][newCol] == 1) {
-                    queue.offer(new int[] { newRow, newCol });
-                    grid[newRow][newCol] = 0;
-                }
-            }
+            if (grid[i][m - 1] == 1)
+                dfs(i, m - 1, grid);
         }
 
         var cnt = 0;
+
         for (var i = 0; i < n; i++)
             for (var j = 0; j < m; j++)
                 if (grid[i][j] == 1)
@@ -58,4 +29,18 @@ class Solution {
         return cnt;
     }
 
+    private void dfs(int row, int col, int[][] grid) {
+        var n = grid.length;
+        var m = grid[0].length;
+
+        if (row < 0 || row >= n || col < 0 || col >= m || grid[row][col] != 1)
+            return;
+
+        grid[row][col] = 0;
+
+        dfs(row + 1, col, grid);
+        dfs(row - 1, col, grid);
+        dfs(row, col + 1, grid);
+        dfs(row, col - 1, grid);
+    }
 }
