3e3084afb3ed4b2590d66e31a3fbf2d53ca13e6f
Hedayatullah Yamin <67416501+Munib03@users.noreply.github.com>
2025-11-10 09:00:50 +0430

    Time: 88 ms (5.08%) | Memory: 69.1 MB (5.49%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.java
+++ 820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -1,10 +1,15 @@
 class Solution {
-    static class UnWeightedGraph {
-        private static class Node {
+    private class UnWeightedUnDirectedGraph {
+        private class Node {
             private final int label;
 
             private Node(int label) {
                 this.label = label;
+            }
+
+            @Override
+            public String toString() {
+                return label + "";
             }
         }
 
@@ -28,33 +33,34 @@ class Solution {
             adjacencyList.get(fromNode).add(toNode);
         }
 
-        private Set<Integer> hasCycle() {
+        public Set<Integer> hasCycle() {
             var visited = new HashSet<Node>();
-            var visiting = new TreeSet<Integer>();
+            var visiting = new HashSet<Integer>();
 
-            for (var node : nodesMap.values())
-                hasCycle(node, visiting, visited);
+            for (var node : nodesMap.values()) {
+                hasCycle(node, visited, visiting);
+            }
 
             return visiting;
         }
 
         // Inner Details of the class
-        private boolean hasCycle(Node node, Set<Integer> visiting, Set<Node> visited) {
+        private boolean hasCycle(Node node, Set<Node> visited, Set<Integer> visiting) {
             visiting.add(node.label);
 
             for (var neighbor : adjacencyList.get(node)) {
                 if (visited.contains(neighbor))
                     continue;
 
-                else if (visiting.contains(neighbor.label))
+                if (visiting.contains(neighbor.label))
                     return true;
 
-                if (hasCycle(neighbor, visiting, visited))
+                else if (hasCycle(neighbor, visited, visiting))
                     return true;
             }
 
-            visited.add(node);
             visiting.remove(node.label);
+            visited.add(node);
 
             return false;
         }
@@ -63,16 +69,20 @@ class Solution {
     public List<Integer> eventualSafeNodes(int[][] graph) {
         var n = graph.length;
 
-        var theRealGraph = new UnWeightedGraph();
-        for (var i = 0; i < n; i++)
-            theRealGraph.addNode(i);
+        var theGraph = new UnWeightedUnDirectedGraph();
 
         for (var i = 0; i < n; i++)
-            for (var j = 0; j < graph[i].length; j++)
-                theRealGraph.addEdge(i, graph[i][j]);
+            theGraph.addNode(i);
 
-        var notSafeNodes = theRealGraph.hasCycle();
+        for (var i = 0; i < n; i++) {
+            for (var j = 0; j < graph[i].length; j++) {
+                theGraph.addEdge(i, graph[i][j]);
+            }
+        }
+
         var list = new ArrayList<Integer>();
+        var notSafeNodes = theGraph.hasCycle();
+
         for (var i = 0; i < n; i++)
             if (!notSafeNodes.contains(i))
                 list.add(i);
