55ab9304f514d74f142ee800e9cb89c2126fb39f
Hedayatullah Yamin <67416501+Munib03@users.noreply.github.com>
2025-11-13 20:25:47 +0430

    Time: 9 ms (12.65%) | Memory: 47.9 MB (5.18%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -1,111 +1,114 @@
 class Solution {
-    private class UnWeightedGraph {
-        private class Node {
-            private int label;
+private class UnWeightedGraph {
+  private class Node {
+    private final int label;
 
-            private Node(int label) {
-                this.label = label;
-            }
-        }
-
-        private final Map<Integer, Node> nodesMap = new HashMap<>();
-        private final Map<Node, List<Node>> adjacencyList = new HashMap<>();
-
-        public void addNode(int label) {
-            var newNode = new Node(label);
-
-            nodesMap.putIfAbsent(label, newNode);
-            adjacencyList.putIfAbsent(newNode, new ArrayList<>());
-        }
-
-        public void addEdge(int from, int to) {
-            var fromNode = nodesMap.get(from);
-            var toNode = nodesMap.get(to);
-
-            if (fromNode == null || toNode == null)
-                return;
-
-            adjacencyList.get(fromNode).add(toNode);
-        }
-
-        public boolean hasCycle() {
-            var visited = new HashSet<Node>();
-            var visiting = new HashSet<Node>();
-
-            for (var node : nodesMap.values())
-                if (!visited.contains(node))
-                    if (hasCycle(node, visiting, visited))
-                        return true;
-
-            return false;
-        }
-
-        public int[] topologicalSort() {
-            var set = new HashSet<Node>();
-            var stack = new Stack<Integer>();
-
-            for (var node : nodesMap.values())
-                if (!set.contains(node))
-                    topologicalSort(node, set, stack);
-
-            int[] ans = new int[stack.size()];
-            var index = 0;
-
-            while (!stack.isEmpty())
-                ans[index++] = stack.pop();
-
-            return ans;
-        }
-
-        // Inner Detaisl of the class
-        private void topologicalSort(Node node, Set<Node> set, Stack<Integer> stack) {
-            if (node == null || set.contains(node))
-                return;
-
-            set.add(node);
-
-            for (var neighbor : adjacencyList.get(node))
-                if (!set.contains(neighbor))
-                    topologicalSort(neighbor, set, stack);
-
-            stack.push(node.label);
-        }
-
-        private boolean hasCycle(Node node, Set<Node> visiting, Set<Node> visited) {
-            visiting.add(node);
-
-            for (var neighbor : adjacencyList.get(node)) {
-                if (visited.contains(neighbor))
-                    continue;
-
-                else if (visiting.contains(neighbor))
-                    return true;
-
-                if (hasCycle(neighbor, visiting, visited))
-                    return true;
-            }
-
-            visited.add(node);
-            visiting.remove(node);
-
-            return false;
-        }
+    private Node(int label) {
+      this.label = label;
     }
 
-    public int[] findOrder(int numCourses, int[][] prerequisites) {
-        var graph = new UnWeightedGraph();
-
-        for (var i = 0; i < numCourses; i++)
-            graph.addNode(i);
-
-        for (var edge : prerequisites) {
-            graph.addEdge(edge[1], edge[0]);
-        }
-
-        if (graph.hasCycle())
-            return new int[] {};
-
-        return graph.topologicalSort();
+    @Override
+    public String toString() {
+      return label + "";
     }
+  }
+
+  private final Map<Integer, Node> nodesMap = new HashMap<>();
+  private final Map<Node, List<Node>> adjacencyList = new HashMap<>();
+
+
+  public void addNode(int val) {
+    var newNode = new Node(val);
+
+    nodesMap.putIfAbsent(val, newNode);
+    adjacencyList.putIfAbsent(newNode, new ArrayList<>());
+  }
+
+  public void addEdge(int from, int to) {
+    var fromNode = nodesMap.get(from);
+    var toNode = nodesMap.get(to);
+
+    if (fromNode == null || toNode == null)
+      return;
+
+    adjacencyList.get(fromNode).add(toNode);
+  }
+
+  public int[] topoSort() {
+    var stack = new Stack<Node>();
+    var set = new HashSet<Node>();
+
+    for (var node : nodesMap.values()) {
+      if (!set.contains(node))
+        topoSort(node, set, stack);
+    }
+
+    var n = stack.size();
+    int[] ans = new int[n];
+    var index = 0;
+
+    while (!stack.isEmpty())
+      ans[index++] = stack.pop().label;
+
+    return ans;
+  }
+
+  private boolean hasCycle() {
+    var visiting = new HashSet<Node>();
+    var visited = new HashSet<Node>();
+    
+    for (var node: nodesMap.values())
+      if (hasCycle(node, visiting, visited))
+        return true;
+    
+    return false;
+  }
+  
+  // Inner Details of the class
+  private boolean hasCycle(Node node, Set<Node> visiting, Set<Node> visited) {
+    visiting.add(node);
+    
+    for (var neighbor: adjacencyList.get(node)) {
+      if (visited.contains(neighbor))
+        continue;
+      
+      if (visiting.contains(neighbor) || hasCycle(neighbor, visiting, visited))
+        return true;
+    }
+    
+    visiting.remove(node);
+    visited.add(node);
+    
+    return false;
+  }
+  
+  private void topoSort(Node node, Set<Node> set, Stack<Node> stack) {
+    if (node == null || set.contains(node))
+      return;
+
+    set.add(node);
+
+    for (var neighbor : adjacencyList.get(node))
+      if (!set.contains(neighbor))
+        topoSort(neighbor, set, stack);
+
+    stack.push(node);
+  }
+}
+
+public int[] findOrder(int numCourses, int[][] prerequisites) {
+  var graph = new UnWeightedGraph();
+
+  for (var i = 0; i < numCourses; i++)
+    graph.addNode(i);
+
+  for (var edge : prerequisites)
+    graph.addEdge(edge[1], edge[0]);
+
+  if (graph.hasCycle())
+    return new int[] {  };
+  
+  return graph.topoSort();
+}
 
 }
