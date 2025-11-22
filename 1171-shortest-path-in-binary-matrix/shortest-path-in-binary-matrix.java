4456efe9bb86272479e9c04d3bf2e0dcd755d37e
Hedayatullah Yamin <67416501+Munib03@users.noreply.github.com>
2025-11-22 12:07:59 +0430

    Time: 134 ms (5.04%) | Memory: 48.3 MB (5.75%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -1,54 +1,53 @@
 class Solution {
-public int shortestPathBinaryMatrix(int[][] grid) {
-  var n = grid.length;
-  var m = grid[0].length;
+    public int shortestPathBinaryMatrix(int[][] grid) {
+        var n = grid.length;
+        var m = grid[0].length;
 
-  if (grid[0][0] == 1 || grid[n - 1][m - 1] == 1)
-    return -1;
+        if (grid[0][0] == 1 || grid[n - 1][m - 1] == 1)
+            return -1;
 
-  var path = 1;
+        var cnt = 1;
+        var queue = new ArrayDeque<Pair>();
+        var set = new HashSet<String>();
 
-  var set = new HashSet<String>();
-  set.add("0,0");
+        set.add("0,0");
+        queue.offer(new Pair(0, 0));
 
-  var queue = new LinkedList<Pair>();
-  queue.offer(new Pair(0, 0));
+        int[] rows = { -1, -1, -1, 0, 0, 1, 1, 1 };
+        int[] cols = { -1, 0, 1, -1, 1, -1, 0, 1 };
 
-  int[] rows = {-1, -1, -1, 0, 0, 1, 1, 1};
-  int[] cols = {-1, 0, 1, -1, 1, -1, 0, 1};
+        while (!queue.isEmpty()) {
+            var currLevelSize = queue.size();
 
-  while (!queue.isEmpty()) {
-    var size = queue.size();
+            while (currLevelSize-- > 0) {
+                var topOfQueue = queue.poll();
+                var row = topOfQueue.row;
+                var col = topOfQueue.col;
 
-    while (size-- > 0) {
-      var topOfQueue = queue.poll();
-      var row = topOfQueue.row;
-      var col = topOfQueue.col;
+                if (row == n - 1 && col == m - 1)
+                    return cnt;
 
-      if (row == n - 1 && col == m - 1)
-        return path;
+                for (var i = 0; i < rows.length; i++) {
+                    var newRow = row + rows[i];
+                    var newCol = col + cols[i];
 
-      for (var i = 0; i < rows.length; i++) {
-        var newRow = row + rows[i];
-        var newCol = col + cols[i];
+                    var key = newRow + "," + newCol;
+                    if (newRow >= 0 && newRow < n && newCol >= 0 && newCol < m && !set.contains(key)
+                            && grid[newRow][newCol] == 0) {
+                        queue.offer(new Pair(newRow, newCol));
+                        set.add(key);
+                    }
+                }
+            }
 
-        var key = newRow + ", " + newCol;
-
-        if (newRow >= 0 && newRow < n && newCol >= 0 && newCol < m && grid[newRow][newCol] == 0 && !set.contains(key)) {
-          queue.offer(new Pair(newRow, newCol));
-          set.add(key);
+            cnt++;
         }
-      }
+
+        return -1;
     }
 
-    path++;
-  }
+    private record Pair(int row, int col) {
 
-  return -1;
-}
-
-private record Pair(int row, int col) {
-
-}
+    }
 
 }
