126fd7a4200e2a089d80fbf724d2a32bf53e26fa
Hedayatullah Yamin <67416501+Munib03@users.noreply.github.com>
2025-11-25 14:25:33 +0430

    Time: 147 ms (5.05%) | Memory: 49.1 MB (6.48%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -6,48 +6,50 @@ class Solution {
         if (grid[0][0] == 1 || grid[n - 1][m - 1] == 1)
             return -1;
 
-        var cnt = 1;
-        var queue = new ArrayDeque<Pair>();
-        var set = new HashSet<String>();
+        else if (n == 1 && grid[0][0] == 0)
+            return 1;
 
-        set.add("0,0");
-        queue.offer(new Pair(0, 0));
+        var set = new HashSet<String>();
+        var queue = new LinkedList<Pair>();
+
+        int[][] destination = new int[n][m];
+        for (var dist : destination)
+            Arrays.fill(dist, Integer.MAX_VALUE);
+
+        set.add(0 + ", " + 0);
+        queue.offer(new Pair(0, 0, 0));
+        destination[0][0] = 0;
 
         int[] rows = { -1, -1, -1, 0, 0, 1, 1, 1 };
         int[] cols = { -1, 0, 1, -1, 1, -1, 0, 1 };
 
         while (!queue.isEmpty()) {
-            var currLevelSize = queue.size();
+            var topOfQueue = queue.poll();
 
-            while (currLevelSize-- > 0) {
-                var topOfQueue = queue.poll();
-                var row = topOfQueue.row;
-                var col = topOfQueue.col;
+            var row = topOfQueue.row;
+            var col = topOfQueue.col;
 
-                if (row == n - 1 && col == m - 1)
-                    return cnt;
+            for (var i = 0; i < rows.length; i++) {
+                var newRow = row + rows[i];
+                var newCol = col + cols[i];
+                var currNumOfSteps = 1 + topOfQueue.numberOfSteps;
 
-                for (var i = 0; i < rows.length; i++) {
-                    var newRow = row + rows[i];
-                    var newCol = col + cols[i];
+                var key = newRow + ", " + newCol;
+                if (newRow >= 0 && newRow < n && newCol >= 0 && newCol < m && !set.contains(key) &&
+                        grid[newRow][newCol] == 0 && currNumOfSteps < destination[newRow][newCol]) {
+                    if (newRow == n - 1 && newCol == m - 1)
+                        return currNumOfSteps + 1;
 
-                    var key = newRow + "," + newCol;
-                    if (newRow >= 0 && newRow < n && newCol >= 0 && newCol < m && !set.contains(key)
-                            && grid[newRow][newCol] == 0) {
-                        queue.offer(new Pair(newRow, newCol));
-                        set.add(key);
-                    }
+                    queue.offer(new Pair(newRow, newCol, currNumOfSteps));
+                    set.add(key);
                 }
             }
-
-            cnt++;
         }
 
         return -1;
     }
 
-    private record Pair(int row, int col) {
+    private record Pair(int row, int col, int numberOfSteps) {
 
     }
-
 }
