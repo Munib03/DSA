edca93e84ea40fe0f9e9bd558a6d870a6bf921fa
Hedayatullah Yamin <67416501+Munib03@users.noreply.github.com>
2025-12-03 09:50:11 +0430

    Time: 0 ms (100.00%) | Memory: 42 MB (23.40%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,17 +1,13 @@
 class Solution {
-public int climbStairs(int n) {
-  return ans(n, new HashMap<>());
-}
+    public int climbStairs(int n) {
+        int[] tab = new int[n + 1];
+        tab[0] = 1;
+        tab[1] = 1;
 
-private int ans(int n, Map<Integer, Integer> map) {
-  if (n <= 2)
-    return n;
+        for (var i = 2; i <= n; i++)
+            tab[i] = tab[i - 1] + tab[i - 2];
 
-  else if (map.containsKey(n))
-    return map.get(n);
+        return tab[n];
+    }
 
-  map.put(n, ans(n - 1, map) + ans(n - 2, map));
-
-  return map.get(n);
-}
 }
