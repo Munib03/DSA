ac983096008495626dcb3b4f15561dc0e9787add
Hedayatullah Yamin <67416501+Munib03@users.noreply.github.com>
2025-12-23 20:06:00 +0430

    Time: 48 ms (6.01%) | Memory: 153.9 MB (12.50%) - LeetSync

--- 4018-minimum-moves-to-balance-circular-array/minimum-moves-to-balance-circular-array.java
+++ 4018-minimum-moves-to-balance-circular-array/minimum-moves-to-balance-circular-array.java
@@ -1,48 +1,69 @@
 class Solution {
     public long minMoves(int[] balance) {
+        var sum = 0L;
+        var theNegNumIndex = Integer.MAX_VALUE;
+        for (var i = 0; i < balance.length; i++) {
+            if (balance[i] < 0)
+                theNegNumIndex = i;
+
+            sum += balance[i];
+        }
+
+        if (sum < 0)
+            return -1;
+
+        else if (theNegNumIndex == Integer.MAX_VALUE)
+            return 0;
+
+        var theDistanceArr = distancesToNegative(balance);
+        var theNum = balance[theNegNumIndex];
+
+        var cnt = 0L;
+
+        for (var i = 1; i < theDistanceArr.length; i++) {
+            var currNum = theDistanceArr[i][0];
+            var theDistance = theDistanceArr[i][1];
+
+            if (currNum <= 0)
+                continue;
+
+            if (currNum < Math.abs(theNum)) {
+                cnt += currNum * theDistance;
+                theNum += (int) currNum;
+            } else {
+                cnt += Math.abs(theNum) * theDistance;
+                break;
+            }
+        }
+
+        return cnt;
+    }
+
+    private long[][] distancesToNegative(int[] balance) {
         int n = balance.length;
-        int neg = -1;
-        long need = 0, have = 0;
+        int negIdx = -1;
 
         for (int i = 0; i < n; i++) {
             if (balance[i] < 0) {
-                neg = i;
-                need = -balance[i];
-            } else {
-                have += balance[i];
+                negIdx = i;
+                break;
             }
         }
 
-        if (need == 0)
-            return 0;
-        if (have < need)
-            return -1;
+        long[][] res = new long[n][2];
 
-        long moves = 0;
-        int d = 1;
+        for (int i = 0; i < n; i++) {
+            int clockwise = (i - negIdx + n) % n;
+            int anticlockwise = (negIdx - i + n) % n;
+            int dist = Math.min(clockwise, anticlockwise);
 
-        while (need > 0) {
-            int left = (neg - d + n) % n;
-            int right = (neg + d) % n;
-
-            if (balance[left] > 0) {
-                long take = Math.min(balance[left], need);
-                moves += take * d;
-                need -= take;
-                balance[left] -= take;
-            }
-
-            if (need > 0 && balance[right] > 0) {
-                long take = Math.min(balance[right], need);
-                moves += take * d;
-                need -= take;
-                balance[right] -= take;
-            }
-
-            d++;
+            res[i][0] = balance[i];
+            res[i][1] = dist;
         }
 
-        return moves;
+        Arrays.sort(res, Comparator.comparingLong(a -> a[1]));
+
+        return res;
     }
 
 }
