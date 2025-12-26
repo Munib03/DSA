0356e548487fad39016d22c40c758208d7329679
Hedayatullah Yamin <67416501+Munib03@users.noreply.github.com>
2025-12-26 20:44:05 +0430

    Time: 3 ms (4.47%) | Memory: 46.6 MB (5.99%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
@@ -1,24 +1,26 @@
 class Solution {
-public int minCostClimbingStairs(int[] cost) {
-  var ans1 = minClimbingStairs(0, cost, new HashMap<>());
-  var ans2 = minClimbingStairs(1, cost, new HashMap<>());
+    public int minCostClimbingStairs(int[] cost) {
+        var memoMap = new HashMap<Integer, Integer>();
 
-  return Math.min(ans1, ans2);
-}
+        var ans1 = minClimbingStairs(0, cost, memoMap);
+        var ans2 = minClimbingStairs(1, cost, memoMap);
 
-private int minClimbingStairs(int index, int[] costs, Map<Integer, Integer> map) {
-  if (index >= costs.length)
-    return 0;
+        return Math.min(ans1, ans2);
+    }
 
-  else if (map.containsKey(index))
-    return map.get(index);
+    private int minClimbingStairs(int index, int[] costs, Map<Integer, Integer> map) {
+        if (index >= costs.length)
+            return 0;
 
-  var oneStep = costs[index] + minClimbingStairs(index + 1, costs, map);
-  var twoStep = costs[index] + minClimbingStairs(index + 2, costs, map);
+        else if (map.containsKey(index))
+            return map.get(index);
 
-  var min = Math.min(oneStep, twoStep);
-  map.put(index, min);
+        var oneStep = costs[index] + minClimbingStairs(index + 1, costs, map);
+        var twoStep = costs[index] + minClimbingStairs(index + 2, costs, map);
 
-  return min;
-}
+        var min = Math.min(oneStep, twoStep);
+        map.put(index, min);
+
+        return min;
+    }
 }
