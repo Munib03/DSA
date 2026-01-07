83317691ab9dc05676629508f156462a4137f43a
Hedayatullah Yamin <67416501+Munib03@users.noreply.github.com>
2026-01-07 07:56:13 +0430

    Time: 5 ms (99.22%) | Memory: 59.2 MB (85.60%) - LeetSync

---
+++ 1465-maximum-product-of-splitted-binary-tree/maximum-product-of-splitted-binary-tree.java
@@ -0,0 +1,31 @@
+class Solution {
+    private long totalSum = 0;
+    private long maxProduct = 0;
+    private static final int MOD = 1_000_000_007;
+
+    public int maxProduct(TreeNode root) {
+        totalSum = getTotalSum(root);
+        computeSubtreeSum(root);
+        return (int) (maxProduct % MOD);
+    }
+
+    private long getTotalSum(TreeNode node) {
+        if (node == null)
+            return 0;
+        return node.val + getTotalSum(node.left) + getTotalSum(node.right);
+    }
+
+    private long computeSubtreeSum(TreeNode node) {
+        if (node == null)
+            return 0;
+
+        long left = computeSubtreeSum(node.left);
+        long right = computeSubtreeSum(node.right);
+        long subSum = node.val + left + right;
+
+        long product = subSum * (totalSum - subSum);
+        maxProduct = Math.max(maxProduct, product);
+
+        return subSum;
+    }
+}
