be5b8f0a8977b4a28110381d931512692a862ca1
Hedayatullah Yamin <67416501+Munib03@users.noreply.github.com>
2026-01-10 16:26:02 +0430

    Time: 9 ms (11.00%) | Memory: 46.8 MB (8.26%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
@@ -1,35 +1,39 @@
 class Solution {
     public int minDistance(String word1, String word2) {
-        int lcs = longestCommonSubsequence(word1, word2);
-        return word1.length() + word2.length() - 2 * lcs;
+        var n = word1.length();
+        var m = word2.length();
+
+        int[][] dp = new int[n + 1][m + 1];
+        for (var sth : dp)
+            Arrays.fill(sth, -1);
+
+        return matchNotMatch(0, 0, word1, word2, dp);
     }
 
-    public int longestCommonSubsequence(String text1, String text2) {
-        int n = text1.length();
-        int m = text2.length();
+    private int matchNotMatch(int index1, int index2, String s1, String s2, int[][] dp) {
+        if (index1 == s1.length())
+            return s2.length() - index2;
 
-        int[][] dp = new int[n][m];
-        for (int[] row : dp)
-            Arrays.fill(row, -1);
+        else if (index2 == s2.length())
+            return s1.length() - index1;
 
-        return matchNotMatch(0, 0, text1, text2, dp);
-    }
+        else if (dp[index1][index2] != -1)
+            return dp[index1][index2];
 
-    private int matchNotMatch(int i, int j, String s, String t, int[][] dp) {
-        if (i == s.length() || j == t.length())
-            return 0;
+        var me = s1.charAt(index1);
+        var ou = s2.charAt(index2);
 
-        if (dp[i][j] != -1)
-            return dp[i][j];
+        if (me == ou)
+            dp[index1][index2] = matchNotMatch(index1 + 1, index2 + 1, s1, s2, dp);
 
-        if (s.charAt(i) == t.charAt(j)) {
-            dp[i][j] = 1 + matchNotMatch(i + 1, j + 1, s, t, dp);
-        } else {
-            dp[i][j] = Math.max(
-                    matchNotMatch(i + 1, j, s, t, dp),
-                    matchNotMatch(i, j + 1, s, t, dp));
+        else {
+            var deleteA = 1 + matchNotMatch(index1 + 1, index2, s1, s2, dp);
+            var deleteB = 1 + matchNotMatch(index1, index2 + 1, s1, s2, dp);
+
+            var min = Math.min(deleteA, deleteB);
+            dp[index1][index2] = min;
         }
 
-        return dp[i][j];
+        return dp[index1][index2];
     }
 }
