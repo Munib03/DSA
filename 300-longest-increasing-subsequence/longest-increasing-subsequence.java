43978d6e7bc2ea0386fee40a73ec4054c2503ae7
Hedayatullah Yamin <67416501+Munib03@users.noreply.github.com>
2026-01-13 17:59:06 +0430

    Time: 392 ms (9.11%) | Memory: 237.4 MB (6.25%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,33 +1,30 @@
 class Solution {
-public int lengthOfLIS(int[] nums) {
-  var n = nums.length;
+    public int lengthOfLIS(int[] nums) {
+        var n = nums.length;
 
-  int[][] dp = new int[n + 1][n + 1];
-  for (var sth : dp)
-    Arrays.fill(sth, -1);
+        int[][] dp = new int[n + 1][n + 1];
+        for (var sth : dp)
+            Arrays.fill(sth, -1);
 
-  var prev = -1;
+        return matchNotMatch(0, -1, nums, dp);
+    }
 
-  return takeNotTake(0, prev, nums, dp);
-}
+    private int matchNotMatch(int index, int prev, int[] nums, int[][] dp) {
+        if (index == nums.length)
+            return 0;
 
-private int takeNotTake(int index, int prev, int[] nums, int[][] dp) {
-  if (index == nums.length)
-    return 0;
+        else if (dp[index][prev + 1] != -1)
+            return dp[index][prev + 1];
 
-  else if (dp[index][prev + 1] != -1)
-    return dp[index][prev + 1];
+        var skip = matchNotMatch(index + 1, prev, nums, dp);
+        var take = 0;
 
-  var skip = takeNotTake(index + 1, prev, nums, dp);
+        if (prev == -1 || nums[index] > nums[prev])
+            take = 1 + matchNotMatch(index + 1, index, nums, dp);
 
-  var take = 0;
-  if (prev == -1 || nums[index] > nums[prev])
-    take = 1 + takeNotTake(index + 1, index, nums, dp);
+        var max = Math.max(skip, take);
+        dp[index][prev + 1] = max;
 
-  var max = Math.max(skip, take);
-  dp[index][prev + 1] = max;
-
-  return dp[index][prev + 1];
-}
-
+        return max;
+    }
 }
