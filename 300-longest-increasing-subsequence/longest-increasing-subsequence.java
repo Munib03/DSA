1a6a938a410261e8cb76ffeb8510a7f3699f692c
Hedayatullah Yamin <67416501+Munib03@users.noreply.github.com>
2026-01-13 15:05:40 +0430

    Time: 397 ms (8.15%) | Memory: 237 MB (18.13%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,18 +1,33 @@
 class Solution {
-    public int lengthOfLIS(int[] nums) {
-        int n = nums.length;
-        int[] dp = new int[n];
-        Arrays.fill(dp, 1);
+public int lengthOfLIS(int[] nums) {
+  var n = nums.length;
 
-        int ans = 1;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (nums[j] < nums[i]) {
-                    dp[i] = Math.max(dp[i], 1 + dp[j]);
-                }
-            }
-            ans = Math.max(ans, dp[i]);
-        }
-        return ans;
-    }
+  int[][] dp = new int[n + 1][n + 1];
+  for (var sth : dp)
+    Arrays.fill(sth, -1);
+
+  var prev = -1;
+
+  return takeNotTake(0, prev, nums, dp);
+}
+
+private int takeNotTake(int index, int prev, int[] nums, int[][] dp) {
+  if (index == nums.length)
+    return 0;
+
+  else if (dp[index][prev + 1] != -1)
+    return dp[index][prev + 1];
+
+  var skip = takeNotTake(index + 1, prev, nums, dp);
+
+  var take = 0;
+  if (prev == -1 || nums[index] > nums[prev])
+    take = 1 + takeNotTake(index + 1, index, nums, dp);
+
+  var max = Math.max(skip, take);
+  dp[index][prev + 1] = max;
+
+  return dp[index][prev + 1];
+}
+
 }
