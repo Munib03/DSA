dfdaef6a4591232919bec09acb199ddd1be586c6
Hedayatullah Yamin <67416501+Munib03@users.noreply.github.com>
2026-01-23 10:36:09 +0430

    Time: 38 ms (10.64%) | Memory: 47.6 MB (9.44%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,38 +1,39 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int n = coins.length;
-        if (amount == 0)
-            return 0;
+        var n = coins.length;
 
         int[][] dp = new int[n][amount + 1];
-        for (int[] row : dp)
-            Arrays.fill(row, -1);
+        for (var sth : dp)
+            Arrays.fill(sth, -1);
 
-        int ans = pickNotPick(0, 0, coins, amount, dp);
+        var ans = pickNotPick(0, 0, coins, amount, dp);
+
         return ans == Integer.MAX_VALUE ? -1 : ans;
     }
 
-    private int pickNotPick(int index, int currSum, int[] nums,
-                            int target, int[][] dp) {
-
-        if (currSum == target)
-            return 0;
-
+    private int pickNotPick(int index, int currSum, int[] nums, int target, int[][] dp) {
         if (index >= nums.length || currSum > target)
             return Integer.MAX_VALUE;
 
-        if (dp[index][currSum] != -1)
+        else if (target == currSum)
+            return 0;
+
+        else if (dp[index][currSum] != -1)
             return dp[index][currSum];
 
-        int pick = Integer.MAX_VALUE;
+        var pick = Integer.MAX_VALUE;
         if (nums[index] <= target - currSum) {
             pick = pickNotPick(index, currSum + nums[index], nums, target, dp);
+
             if (pick != Integer.MAX_VALUE)
                 pick += 1;
         }
 
-        int notPick = pickNotPick(index + 1, currSum, nums, target, dp);
+        var notPick = pickNotPick(index + 1, currSum, nums, target, dp);
 
-        return dp[index][currSum] = Math.min(pick, notPick);
+        var min = Math.min(pick, notPick);
+        dp[index][currSum] = min;
+
+        return min;
     }
 }
