10377957ac06311343385c4544d0ac71394268f9
Hedayatullah Yamin <67416501+Munib03@users.noreply.github.com>
2026-01-26 07:58:37 +0430

    Time: 11 ms (5.37%) | Memory: 46.8 MB (11.10%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
@@ -1,38 +1,38 @@
 class Solution {
-public int minDistance(String word1, String word2) {
-  var n = word1.length();
-  var m = word2.length();
-  
-  int[][] dp = new int[n + 1][m + 1];
-  for (var sth: dp)
-    Arrays.fill(sth, -1);
-  
-  return matchNotMatch(0, 0, word1, word2, dp);
-}
+    public int minDistance(String word1, String word2) {
+        var n = word1.length();
+        var m = word2.length();
 
-private int matchNotMatch(int i, int j, String s, String t, int[][] dp) {
-  if (i >= s.length())
-    return t.length() - j;
-  
-  else if (j >= t.length())
-    return s.length() - i;
-  
-  else if (dp[i][j] != -1)
-    return dp[i][j];
-  
-  var me = s.charAt(i);
-  var ou = t.charAt(j);
-  
-  if (me == ou)
-    return matchNotMatch(i + 1, j + 1, s, t, dp);
-  
-  else {
-    var deleteI = 1 + matchNotMatch(i + 1, j, s, t, dp);
-    var deleteJ = 1 + matchNotMatch(i, j + 1, s, t, dp);
-    
-    dp[i][j] = Math.min(deleteI, deleteJ);
-  }
-  
-  return dp[i][j];
-}
+        int[][] dp = new int[n + 1][m + 1];
+        for (var sth : dp)
+            Arrays.fill(sth, -1);
+
+        return matchNotMatch(0, 0, word1, word2, dp);
+    }
+
+    private int matchNotMatch(int i, int j, String s, String t, int[][] dp) {
+        if (i >= s.length())
+            return t.length() - j;
+
+        else if (j >= t.length())
+            return s.length() - i;
+
+        else if (dp[i][j] != -1)
+            return dp[i][j];
+
+        var me = s.charAt(i);
+        var ou = t.charAt(j);
+
+        if (me == ou)
+            return matchNotMatch(i + 1, j + 1, s, t, dp);
+
+        else {
+            var deleteI = 1 + matchNotMatch(i + 1, j, s, t, dp);
+            var deleteJ = 1 + matchNotMatch(i, j + 1, s, t, dp);
+
+            dp[i][j] = Math.min(deleteI, deleteJ);
+        }
+
+        return dp[i][j];
+    }
 }
