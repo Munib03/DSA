ff7d48e24da234c47c86abcb3166ef46a3e0c710
Hedayatullah Yamin <67416501+Munib03@users.noreply.github.com>
2026-01-28 11:13:56 +0430

    Time: 1 ms (71.07%) | Memory: 43.7 MB (25.38%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,36 +1,34 @@
 class Solution {
-    public int maxProfit(int[] prices) {
-        var n = prices.length;
+public int maxProfit(int[] prices) {
+  var n = prices.length;
+  
+  int[][] dp = new int[n][2];
+  for (var sth: dp)
+    Arrays.fill(sth, -1);
+  
+  return buyNotBuy(0, 1, prices, dp);
+}
 
-        int[][] dp = new int[n][2];
-        for (var sth : dp)
-            Arrays.fill(sth, -1);
-
-        return buyNotBuy(0, 1, prices, dp);
-    }
-
-    private int buyNotBuy(int day, int canBuy, int[] prices, int[][] dp) {
-        if (day >= prices.length)
-            return 0;
-
-        else if (dp[day][canBuy] != -1)
-            return dp[day][canBuy];
-
-        var profit = 0;
-        if (canBuy == 1) {
-            var buy = -prices[day] + buyNotBuy(day + 1, 0, prices, dp);
-            var skip = buyNotBuy(day + 1, 1, prices, dp);
-
-            profit = Math.max(buy, skip);
-        } else {
-            var sell = prices[day] + buyNotBuy(day + 2, 1, prices, dp);
-            var skip = buyNotBuy(day + 1, 0, prices, dp);
-
-            profit = Math.max(sell, skip);
-        }
-
-        dp[day][canBuy] = profit;
-
-        return profit;
-    }
+private int buyNotBuy(int index, int canBuy, int[] prices, int[][] dp) {
+  if (index >= prices.length)
+    return 0;
+  
+  else if (dp[index][canBuy] != -1)
+    return dp[index][canBuy];
+  
+  else if (canBuy == 1) {
+    var buy = -prices[index] + buyNotBuy(index + 1, 0, prices, dp);
+    var notBuy = buyNotBuy(index + 1, canBuy, prices, dp);
+    
+    dp[index][canBuy] = Math.max(buy, notBuy);
+  }
+  else {
+    var sell = prices[index] + buyNotBuy(index + 2, 1, prices, dp);
+    var notSell = buyNotBuy(index + 1, canBuy, prices, dp);
+    
+    dp[index][canBuy] = Math.max(sell, notSell);
+  }
+    
+  return dp[index][canBuy];
+}
 }
