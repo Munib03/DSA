91ec32e66f0510dc0d14d9118059582183f4fd4e
Hedayatullah Yamin <67416501+Munib03@users.noreply.github.com>
2026-05-12 21:10:48 +0430

    Time: 10 ms (23.54%) | Memory: 48.7 MB (76.81%) - LeetSync

---
+++ 324-wiggle-sort-ii/wiggle-sort-ii.java
@@ -0,0 +1,20 @@
+class Solution {
+public void wiggleSort(int[] nums) {
+  var n = nums.length;
+  Arrays.sort(nums);
+
+  var index = 1;
+  int[] ansArr = new int[n];
+
+  var i = n-1;
+  for (i=n-1; i >= 0 && index < n; i--, index += 2)
+    ansArr[index] = nums[i];
+
+  index = 0;
+  for (; i >= 0; i--, index += 2)
+    ansArr[index] = nums[i];
+
+  for (var f=0; f < n; f++)
+    nums[f] = ansArr[f];
+}
+}
