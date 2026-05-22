5446c830c31143de3f2bc823b0775341f47ba762
Hedayatullah Yamin <67416501+Munib03@users.noreply.github.com>
2026-05-22 15:23:34 +0430

    Time: 47 ms (5.72%) | Memory: 47.1 MB (12.28%) - LeetSync

--- 871-keys-and-rooms/keys-and-rooms.java
+++ 871-keys-and-rooms/keys-and-rooms.java
@@ -1,71 +1,42 @@
 class Solution {
-    public class UnWeightedGraph {
-        private static class Node {
-            private final int val;
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        var n = rooms.size();
 
-            private Node(int val) {
-                this.val = val;
+        var map = new HashMap<Integer, List<Integer>>();
+        for (var i = 0; i < n; i++)
+            map.put(i, new ArrayList<>());
+
+        for (var i = 0; i < n; i++) {
+            var room = rooms.get(i);
+
+            for (var key : room) {
+                map.get(i).add(key);
             }
-
-            @Override
-            public String toString() {
-                return this.val + "";
-            }
         }
 
-        private final Map<Integer, Node> nodes = new HashMap<>();
-        private final Map<Node, List<Node>> adjacencyList = new HashMap<>();
+        for (var sth : map.entrySet())
+            System.out.println(sth.getKey() + ": " + sth.getValue());
 
-        public void addNode(int val) {
-            var newNode = new Node(val);
+        var ansSet = new HashSet<Integer>();
 
-            nodes.putIfAbsent(val, newNode);
-            adjacencyList.putIfAbsent(newNode, new ArrayList<>());
-        }
+        dfs(0, map, new HashSet<>(), ansSet);
 
-        public void addEdge(int from, int to) {
-            var fromNode = nodes.get(from);
-            var toNode = nodes.get(to);
-
-            if (fromNode == null || toNode == null)
-                throw new IllegalArgumentException();
-
-            adjacencyList.get(fromNode).add(toNode);
-        }
-
-        public boolean canVisitAllRooms(List<List<Integer>> rooms) {
-            var n = rooms.size();
-
-            for (var i = 0; i < n; i++)
-                this.addNode(i);
-
-            for (var i = 0; i < n; i++) {
-                for (var j = 0; j < rooms.get(i).size(); j++) {
-                    this.addEdge(i, rooms.get(i).get(j));
-                }
-            }
-
-            var ansSet = new HashSet<Node>();
-            dfs(nodes.get(0), new HashSet<>(), ansSet);
-
-            return ansSet.size() == n;
-        }
-
-        private void dfs(Node node, Set<Node> set, Set<Node> ansSet) {
-            if (node == null)
-                return;
-
-            set.add(node);
-            ansSet.add(node);
-            for (var neighbor : adjacencyList.get(node))
-                if (!set.contains(neighbor))
-                    dfs(neighbor, set, ansSet);
-        }
+        return ansSet.size() == n;
     }
 
-    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
-        var obj = new UnWeightedGraph();
+    private void dfs(int node,
+            Map<Integer, List<Integer>> map,
+            Set<Integer> visited,
+            Set<Integer> ansSet) {
+        if (visited.contains(node))
+            return;
 
-        return obj.canVisitAllRooms(rooms);
+        visited.add(node);
+        ansSet.add(node);
+
+        for (var neighbor : map.get(node))
+            if (!visited.contains(neighbor))
+                dfs(neighbor, map, visited, ansSet);
+
     }
 }
