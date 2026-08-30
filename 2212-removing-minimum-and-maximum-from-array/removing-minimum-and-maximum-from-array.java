c82da4dea1e91fa2f9dde28c3865f075710c8390
Hedayatullah Yamin <67416501+Munib03@users.noreply.github.com>
2026-08-30 09:24:04 +0430

    Time: 3 ms (61.82%) | Memory: 86.9 MB (8.73%) - LeetSync

--- 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.java
+++ 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.java
@@ -1,23 +1,36 @@
 class Solution {
-    public int minimumDeletions(int[] nums) {
-        var n = nums.length;
+public int minimumDeletions(int[] nums) {
+  var n = nums.length;
 
-        var minIndex = 0;
-        var maxIndex = 0;
+  var minIndex = 0;
+  var maxIndex = 0;
 
-        for (var i=0; i < n; i++) {
-            var num = nums[i];
+  for (var i=0; i < n; i++) {
+    var num = nums[i];
 
-            if (num > nums[maxIndex])
-                maxIndex = i;
+    if (num < nums[minIndex])
+      minIndex = i;
 
-            if (num < nums[minIndex])
-                minIndex = i;
-        }
+    if (num > nums[maxIndex])
+      maxIndex = i;
+  }
 
-        var l = Math.min(minIndex, maxIndex);
-        var r = Math.max(minIndex, maxIndex);
+  // [1, 2, -4, 123, 10]
+  var removeBothFromFront = Math.max(minIndex, maxIndex) + 1;
+  var removeBothFromEnd = n -Math.min(minIndex, maxIndex);
 
-        return Math.min(Math.min(r + 1, n - l), l+ 1 + n - r);
-    }
+  var removeMaxFromFront = maxIndex + 1;
+  var removeMaxFromEnd = n - maxIndex;
+
+  var removeMinFromFront = minIndex + 1;
+  var removeMinFromEnd = n - minIndex;
+
+  var min1 = Math.min(removeMinFromFront, removeMinFromEnd);
+  var min2 = Math.min(removeMaxFromEnd, removeMaxFromFront);
+
+  var min3 = min1 + min2;
+  var min4 = Math.min(removeBothFromEnd, removeBothFromFront);
+
+  return Math.min(min3, min4);
+}
 }
