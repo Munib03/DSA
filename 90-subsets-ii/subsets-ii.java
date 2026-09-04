5ffebe5d7172b3dec53bece9139c6bcd27b41b98
Hedayatullah Yamin <67416501+Munib03@users.noreply.github.com>
2026-09-04 20:23:04 +0430

    Time: 4 ms (16.66%) | Memory: 45 MB (60.10%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -1,28 +1,23 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         Arrays.sort(nums);
-        Set<List<Integer>> ans = new HashSet<>();
+        Set<List<Integer>> set = new HashSet<>();
 
-        backtrack(0, nums, new ArrayList<>(), ans);
+        subset(0, nums, set, new ArrayList<>());
 
-        return new ArrayList<>(ans);
+        return new ArrayList<>(set);
     }
 
-    private void backtrack(int index, int[] nums, List<Integer> list, Set<List<Integer>> ansList) {
-        if (index == nums.length) {
-            ansList.add(new ArrayList<>(list));
+    public void subset(int index, int[] nums, Set<List<Integer>> set, List<Integer> list) {
+        if (index >= nums.length) {
+            set.add(new ArrayList<>(list));
             return;
         }
 
         list.add(nums[index]);
-        backtrack(index + 1, nums, list, ansList);
-
-        var nextIndex = index;
-        while (nextIndex < nums.length && nums[index] == nums[nextIndex])
-            nextIndex++;
+        subset(index + 1, nums, set, list);
 
         list.removeLast();
-        backtrack(nextIndex, nums, list, ansList);
+        subset(index + 1, nums, set, list);
     }
-
 }
